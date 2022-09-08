a84e36e04e508eaf8e60fda2d50f0b793ed74cca
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-09-09 00:53:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,48 @@
+const fs = require('fs');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 module.exports = {
-  profile: {
-    ownerName: "", // defult: AiDarkEzio
-    ownerNumb: "", // defult: 94761539856
-    botName: "", // defult: Whats Bot MD
+    VERSION: 'V 1.0.0',
+    SESSION_ID: process.env.SESSION_ID || '',
+    U_STATUS: process.env.U_STATUS || 'true',
+    MENTION: process.env.MENTION || 'on',
+    WORKTYPE: process.env.WORKTYPE || 'public',
+    OWNER : ["917025099154"],
+    BRANCH: 'master',
+    SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
+    HEROKU: {
+        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
+        API_KEY: process.env.HEROKU_API_KEY || '',
+        APP_NAME: process.env.HEROKU_APP_NAME || ''
+    },
+    profile: {
+    ownerName: "ɪɴʀʟ-ʙᴏᴛ", 
+    ownerNumb: "917025099154", 
+    botName: "ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ", 
   },
-  reply: {
-    notFound: "", // defult: "This Command not created. it was creating",
-    success: "", // defult: "Done ✓",
-    admin: "", // defult: "This Feature Is Only For Admin!",
-    botAdmin: "", // defult: "Bot Must Be Admin First!",
-    owner: "", // defult: "This Feature Is Only For Owner!",
-    group: "", // defult: "Feature Used Only For Groups!",
-    private: "", // defult: "Features Used Only For Private Chat!",
-    bot: "", // defult: "This Feature Is Only For Bot",
-    wait: "", // defult: "In process...",
-    linkm: "", // defult: "Where is the link?",
-    error: "", // defult: "Error!!",
-    endLimit: "", // defult: "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours",
-    ban: "", // defult: "You have been banned by the owner, if you want to be unbanned, chat owner.",
-    nsfw: "", // defult: "The nsfw feature has not been activated, please contact the admin to activate",
-    banChat: "", // defult: "The bot was banned in this group, please contact the owner to unban",
+    reply: {
+    notFound: "This Command not created. it was creating",
+    success: "Done ✓",
+    admin: "This Feature Is Only For Admin",
+    botAdmin: "Bot Must Be Admin First", 
+    owner: "This Feature Is Only For Owner",
+    group: "Feature Used Only For Groups!", 
+    private: "Features Used Only For Private Chat!", 
+    bot: "This Feature Is Only For Bot", 
+    wait: "In process..",
+    linkm: "", 
+    error: "Error!!", 
+    endLimit: "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours",
+    ban: "You have been banned by the owner, if you want to be unbanned, chat owner", 
+    nsfw: "The nsfw feature has not been activated, please contact the admin to activate", 
+    banChat: "The bot was banned in this group, please contact the owner to unban", 
   },
-  setting: {
+    setting: {
     blockchat: [], // Your block chat Jids
   },
   auto: {
@@ -35,8 +56,30 @@ module.exports = {
     },
     presence: {
       is: false, // U Can on or off this () | u can set true or false
-      value: "typing", // It has two types | u can set 'recoding' or 'typing'
+      value: "recording", // It has two types | u can set 'recoding' or 'typing'
     },
     read: false, // Boolean | ===== It not created now ======
   },
-};
+    DATABASE_URL: DATABASE_URL,
+    FOOTER : process.env.FOOTER || "ɪɴʀʟ-ᴍᴅ",
+    PACKNAME : process.env.PACKNAME || "ɪɴʀʟ-ʙᴏᴛ",
+    GIT : process.env.GIT || "https://tinyurl.com/3ex3e48e",
+    WEB : process.env.WEB || "https://tinyurl.com/ycks3s8p",
+    YT : process.env.YT || "https://www.youtube.com/channel/null",
+    CAPTION : process.env.CAPTION || "*Cerated by inrlTeam*",
+    DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
+    NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
+    SUDO: process.env.SUDO || '917593919575',
+    WAGRP : process.env.WAGRP || 'https://tinyurl.com/dbtjh2vu',
+    DEBUG: DEBUG,
+    ACR_A: "ff489a0160188cf5f0750eaf486eee74",
+    ACR_S: "ytu3AdkCu7fkRVuENhXxs9jsOW4YJtDXimAWMpJp"
+    };
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
