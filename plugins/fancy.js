5f3c1389e8889b5bc90804a2e937052530b5eb02
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-09-09 15:12:02 +0530

    Update fancy.js

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -3,7 +3,7 @@ const {Fancy} = require('abu-bot')
 const {WORKTYPE} = require('../config');
 var w = WORKTYPE == 'public'? false:true
 addCommand({pattern: 'fancy ?(.*)', fromMe: w, desc: 'Creates fancy text fonts'}, (async (message, client) => {
-if (!match[1]||!message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n"+Fancy("example","32"))
+if (!client[1]||!message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n"+Fancy("example","32"))
 try { var result = Fancy(message.reply_message.message,match[1]) } catch (e) {return await message.sendReply(e.message)}
 await message.reply(result)
      }))
