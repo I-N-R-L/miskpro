17f0d21ad8671845856c8489044e07644113a309
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-09-10 04:55:57 +0530

    Fix ✅

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -5,16 +5,16 @@ const Config = require('../config');
 const lang = whatsbixby.getString("weather");
 
 whatsbixby.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: lang.HMODD_DESC, dontAddCommandList: true }, async (message, client) => {
-	if (client[1] === '') return await message.reply(Lang.NEED_APPNAME);
+	if (client[1] === '') return await client.reply(Lang.NEED_APPNAME);
 	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
+		if (response.statusCode === 200) return await client.sendMessage(message.jid, 
 		'*📕 ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n\n' + 
 		'*📘 ' + Lang.SIZE +'* ```' + json.result[0].size + '```\n\n\n' + 
 		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[0].link + '```\n', MessageType.text);
 	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+		return await client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
 });
