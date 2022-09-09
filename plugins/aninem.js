227fc1ef6ccedf6d663aa2063a9e9be2be1f11a6
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-09-09 21:30:12 +0530

    Update aninem.js

--- plugins/aninem.js
+++ plugins/aninem.js
@@ -12,12 +12,7 @@ var r_text = new Array();
 
 
 var i = Math.floor(r_text.length * Math.random());
-    // const buttons = [{buttonId: ".wallpaper",buttonText: { displayText: "Next Wallpaper 🔀⏩" },type: 1,},];
-    const Message = {
-      image: { url: r_text[i] },
-      caption: ezio.config.exif.cap,
-      // footer: ezio.config.exif.footer,
-      // buttons,
+    const Message = {image: { url: r_text[i] },caption: ezio.config.exif.cap,
     };
     await client.sendMessage(message.from, Message, { quoted: message });
     global.catchError = false;
