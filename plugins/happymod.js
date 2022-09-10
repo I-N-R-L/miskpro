e61c2fe10186e6dd60074489ce9c04b0c9e7d12d
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-10 17:59:27 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -1,21 +1,18 @@
-const whatsbixby = require('../events');
-const {MessageType} = require("Wa-Web");
+const andikudamkk = require('../events');
 const got = require('got');
 const Config = require('../config');
-const lang = whatsbixby.getString("weather")
-
-whatsbixby.addCommand( { pattern: ["happymod"],desc: lang.HMODD_DESC, sucReact: "😁",  category: ["all", "create"], }, async (message, client) => {
-
-	if (client[1] === '') return await client.reply(Lang.NEED_APPNAME);
-	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
+const misk = message.client.text;
+andikudamkk.addCommand({pattern: ['happymod'], fromMe: false, desc: "to get mode apk",sucReact: "😁",  category: ["all", "create"],}, async (message, client) => {
+	if (!misk) return await client.sendMessage(message.from, { text :"need an apk name to get mod"},{ quoted: message })
+	const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${misk}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await client.sendMessage(message.jid, 
+		if (response.statusCode === 200) return await client.sendMessage( message.from, { text:
 		'*📕 ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n\n' + 
 		'*📘 ' + Lang.SIZE +'* ```' + json.result[0].size + '```\n\n\n' + 
-		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[0].link + '```\n', MessageType.text);
+		'*📗 ' + Lang.DOWNLOAD +':* ```' + json.result[0].link + '```\n', }, { quoted: message });
 	} catch {
-		return await client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+		return await client.sendMessage( message.from, { text : "no data found on this apk"},{ quoted: message });
 	}
 });
