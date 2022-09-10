c50bfe788b670ea673883c777c26039006427b75
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-09-10 17:26:40 +0530

    Fix✅

--- plugins/fancys.js
+++ plugins/fancys.js
@@ -9,11 +9,11 @@ Jsl.addCommand(
                 category: ["system", "all"],
 	   },
  }, (async (message, client) => {
-     if (!client[1] || !message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n" + Fancy("example", "32"))
+     if (!client[1] || !message.reply_message.message) return await client.sendMessage("Reply to any message with .fancy number\n" + Fancy("example", "32"))
      try {
          var result = Fancy(message.reply_message.message, client[1])
      } catch (e) {
-         return await message.sendReply(e.message)
+         return await client.sendMessage(e.message)
      }
-     await message.sendReply(result)
+     await client.sendMessage(result)
  }))
