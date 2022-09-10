7ffa943f1ea4d299e07eca6eb3ea8e1a1660a1ea
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-09-10 11:10:11 +0530

    Fix✅

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,15 +1,3 @@
-/* ═══════════════════════════════════════════════════════ //
-=> If you want to recode, reupload,
-=> or copy the codes/script,
-=> pls give credit,
-=> no credit? i will take action immediately.
-==> Copyright (C) 2022 Dark_Ezio.
-==> Licensed under the  MIT License;
-===> you may not use this file except in compliance with the License.
-=> Thank you to Lord Buddha, Family and Myself.
-=> Whats Bot - Dark_Ezio.
-// ════════════════════════════ */
-
 const ezio = require("../events");
 const lang = ezio.getString("ttp");
 const axios = require("axios");
