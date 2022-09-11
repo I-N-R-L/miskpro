7730e2b6aefdd0d31432032b0c652632aea91044
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-09-11 19:32:11 +0530

    Fix✅

---
+++ plugins/encrypter.js
@@ -0,0 +1,41 @@
+const whatsbixby = require('../events');
+const {MessageType} = require("Wa-web");
+const axios = require('axios');
+const Config = require('../config');
+
+// ENCRYPTION PLUGIN FOR FUN, IF U NEED COPY PASTE THIS 
+// NO NEED CREDIT
+const b64 = "Text encryption using base64."
+const usage = ".b64en <text>"
+
+const encypt = "```Enter the text which you need to encrypt!```"
+
+whatsbixby.addCommand({ pattern: 'b64en ?(.*)', fromMe: false, desc: b64, usage: usage }, async (message, client) => {
+
+        const Wtb = client[1]
+        
+        if (client[1] === '') return await client.sendMessage(message.from, encypt, MessageType.text);
+
+        await axios
+          .get(`https://xteam.xyz/encrypt/b64enc?APIKEY=ab9942f95c09ca89&text=${Wtb}`)
+          .then(async (response) => {
+            const {
+              status,
+              result,
+            } = response.data
+
+            const msg = `*CONNECTION STATUS ✔:* ${status}\n\n\n *ENCRYPTED TEXT:* ${result}`
+            await client.sendMessage(message.from, msg, MessageType.text)
+           })
+      },
+    )
+
+
+
+
+
+
+
+
+
+
