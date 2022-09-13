092d952c3fc0220b814960d0a016c394afde2ab0
inrlinrlfazfaz-inrl-faz-inrl-faz-3333 <107807702+inrlinrlfazfaz-inrl-faz-inrl-faz-3333@users.noreply.github.com>
2022-09-13 22:25:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,7 @@ require("./global");
 const Config = require('./config');
 const { default: WASocket, DisconnectReason, useSingleFileAuthState, fetchLatestBaileysVersion, jidNormalizedUser, makeInMemoryStore, DEFAULT_CONNECTION_CONFIG, DEFAULT_LEGACY_CONNECTION_CONFIG, } = require("inrl");
 const fs = require("fs");
-var aes256 = require('aes256');
-let PastebinAPI = require('pastebin-js'),
-    pastebin = new PastebinAPI({
-      'api_dev_key' : 'u_53edsqmFGKd02RMyQPwONVG0bIPi-R',});
-const mddc=(Config.SESSION_ID)
-//const mddc= ('inrl~c771a40a74b71f134142d0893799f7e7:5a51754332536255626d3854362b6351)
-var m = (mddc);
-let mdm = m.replaceAll("inrl~", "");
-var key = 'k!t';
-var plaintext = (mdm);
-var decryptedPlainText = aes256.decrypt(key, plaintext);
-
-pastebin
-  .getPaste(decryptedPlainText)
-  .then(function (data) {
-   fs.writeFileSync("./session.json" , data);
-   return;
-  });
+const session = require('./lib/session');
 const chalk = require("chalk");
 const pino = require("pino");
 const path = require("path");
