0ba6144ac62675fd4a91ead12596669e875d2b70
inrlinrlfazfaz-inrl-faz-inrl-faz-3333 <107807702+inrlinrlfazfaz-inrl-faz-inrl-faz-3333@users.noreply.github.com>
2022-09-13 21:11:39 +0530

    Update global.js

--- global.js
+++ global.js
@@ -15,7 +15,7 @@ global.user = require("./lib/database/data/user")
 global.group = require("./lib/database/data/group")
 global._user = JSON.parse(fs.readFileSync("./lib/database/data/user.json"))
 global._group = JSON.parse(fs.readFileSync("./lib/database/data/group.json"))
-global.owner = ["9446072492"];
+global.owner = ["917593919575"];
 global.mess = (type, m) => {
     let msg = { wait: 'Wait, in progress', owner: 'Perintah ini hanya dapat digunakan oleh Owner!', premium: 'Perintah ini hanya dapat digunakan oleh Premium!', group: 'Perintah ini hanya dapat digunakan di group!', private: 'Perintah ini hanya dapat digunakan di private chat!', admin: 'Perintah ini hanya dapat digunakan oleh admin group!', botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut', bot: 'Fitur ini hanya dapat diakses oleh Bot', dead: 'Fitur ini sedang dimatikan!', media: 'Reply media', error: "No Results Found" }[type]
     if (msg) return m.reply(msg, m.from, { quoted: m })
