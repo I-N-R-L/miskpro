7de5697bdf9d9110e5693f1c5f6dd496e87d9df9
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-15 23:22:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,13 +36,14 @@ pastebin
    fs.writeFileSync("./session.json" , data);
 await console.log('file creted successfully☑️');
 });
-await const { state, saveState } = useSingleFileAuthState( "./session.json", pino({ level: "silent" }) );
+  };
+await console.log('successfuly connected to the server');
+const { state, saveState } = useSingleFileAuthState("./session.json")
 const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }),});
 store.readFromFile("./lib/database/json/baileys/store_multi.json");
 setInterval(() => { store.writeToFile("./lib/database/baileys/store_multi.json")}, 30 * 1000);
 fs.readdirSync("./plugins").forEach((file) => {if (path.extname(file).toLowerCase() == ".js") {require(`./plugins/${file}`);}});
 global.api = (name, path = "/", query = {}, apikeyqueryname) => (name in jsoConfig.APIs ? jsoConfig.APIs[name] : name) + path + (query || apikeyqueryname ? "?" + new URLSearchParams( Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: jsoConfig.APIs.apikey } : {}), }) ) : "");
-};
 if('./session.json'!== true ){
 console.log(' session file cretion failed ');
 };
