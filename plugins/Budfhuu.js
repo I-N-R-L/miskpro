326653ca26fc252bd7c487b2e85c6212a6c8091b
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-16 22:26:41 +0530

    Update Budfhuu.js

--- plugins/Budfhuu.js
+++ plugins/Budfhuu.js
@@ -12,7 +12,7 @@ const got = require('got');
 const Config = require('../config');
 
 
-    inrl({pattern: ['weather'], desc: "to check whether", sucReact: "💔", category: ['all'], usage: '<word>',},   async (message, client) => {
+    inrl({pattern: ['ml'], desc: "to check whether", sucReact: "💔", category: ['all'], usage: '<word>',},   async (message, client) => {
            const text = message.client.text;
 	    if (!text) return await client.sendMessage( message.from, { text: 'Enter A location'}, { quoted: message });
 	    const url = `https://api.simsimi.net/v2/?text=${text}&lc=ml`;
