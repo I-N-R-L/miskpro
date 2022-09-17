29f43c954612a51beebb5982990f7dfcf04ee28f
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-17 20:44:53 +0530

    Update Data.js

--- plugins/Data.js
+++ plugins/Data.js
@@ -2,8 +2,6 @@ const bots= require('../lib/perfix');
 const Config = require('../config');
 
 
-   bots.inrl({pattern: ['data'], desc: "to check whether", sucReact: "ðŸ¥¸", category: ['all'],},   async (message, client) => {
-if (!message.quoted) return await client.sendMessage( message.from, { text: 'please mension some one to take their data'}, { quoted: message });
-const ibotmd = `https://docs-jojo.herokuapp.com/api/fake_identity?lang=en&type=ibotmd`;
- return await client.sendMessage(message.from,  { text : 'NAME  :' + ibotmd.name + '\n' + 'GENDER :'+ ibotmd.gender + 'AGE  :' + ibotmd.age + '\n' + 'BDAY :'+ ibotmd.birtday + 'JOB  :' + ibotmd.occupation + '\n' + 'ADDRESS :'+ ibotmd.address +'PIN  :' + ibotmd.zip_code + '\n' + 'PLACE :'+ ibotmd.state + 'COUNTRY  :' + ibotmd.country + '\n' + 'PHONE :'+ ibotmd.phone}, { quoted: message });
+   bots.inrl({pattern: ['@'], desc: "to check whether", sucReact: "❣️", category: ['all'],},   async (message, client) => {
+ await client.sendMessage( message.from, { text: 'please mension some one to take their data'}, { quoted: message });
 });
