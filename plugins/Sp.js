9f7c5366e71fa1fcd2ef194d2a8a4599f002389c
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-17 13:48:26 +0530

    Create Sp.js

---
+++ plugins/Sp.js
@@ -0,0 +1,13 @@
+const bots = require("../lib/perfix");
+const Config = require('../config');
+bots.inrl( { pattern: ["sp"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
+	async (message, client) => {
+	let _message = message.quoted.imageMessage || message.client.text;
+		if (!_message)
+			return await client.sendMessage( message.from,{ text :'*Reply to a image.*'}, { quoted: message })
+			const url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥵_🥲`;
+          const response = await got(url);
+		    const json = JSON.parse(response.body);
+		    if (response.statusCode === 200) return await client.sendImageAsSticker(message.from, json.url, { packname: "inrl", author: "inrl", categories: json.tags })
+	}
+);
