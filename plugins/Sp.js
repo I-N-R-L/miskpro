bedcac18b19100644c13b8ecb8960a326a5f8118
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-17 15:08:00 +0530

    Update Sp.js

--- plugins/Sp.js
+++ plugins/Sp.js
@@ -1,9 +1,26 @@
-const bots = require("../lib/perfix");
-const Config = require('../config');
-bots.inrl( { pattern: ["sp"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
-	async (message, client) => {
-	const url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥵_🥲`;
-const Message = { image: { url:  url.url }, caption: bots.config.exif.cap,  };
+/* Copyright (C) 2020 INrlTeam.
 
-        await client.sendMessage( message.from, Message,{ quoted: message })
-});
+Licensed under the  GPL-3.0 License;
+
+you may not use this file except in compliance with the License.
+
+inrl-md
+
+*/
+const bots = require('../lib/perfix');
+const got = require('got');
+const Config = require('../config');
+
+
+    bots.inrl({pattern: ['sp'], desc: "to check whether", sucReact: "💔", category: ['all'], usage: '<word>',},   async (message, client) => {
+           const text = message.client.text;
+	    if (!text) return await client.sendMessage( message.from, { text: 'Enter A location'}, { quoted: message });
+	    const url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥵_🥲`;
+	    try {
+		    const response = await got(url);
+		    const json = JSON.parse(response.body);
+		    if (response.statusCode === 200) return await client.sendMessage( message.from, { image: { url:  json.url }, caption: bots.config.exif.cap,  };
+	    } catch {
+		    return await client.sendMessage( message.from, { text : "no data found on this location"},{ quoted: message });
+	    }
+    });
