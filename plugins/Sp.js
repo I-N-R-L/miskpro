38f0a0b3016c4e1995816814bfdea8666bbd9180
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-17 14:23:48 +0530

    Update Sp.js

--- plugins/Sp.js
+++ plugins/Sp.js
@@ -6,6 +6,9 @@ bots.inrl( { pattern: ["sp"],desc: 'set full size profile picture', sucReact: "�
 	const url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥵_🥲`;
           const response = await got(url);
 		    const json = JSON.parse(response.body);
-		    if (response.statusCode === 200) return await client.sendImageAsSticker(message.from, json.url, { packname: "inrl", author: "inrl", categories: json.tags })
-	}
-);
+		    if (response.statusCode === 200)
+const Message = {
+      image: { url: json.url },
+      caption:`╭`
+return await client.sendImageAsSticker(message.from, Message, { quoted: message });
+});
