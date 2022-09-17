779f87f61de0f9ae45fd7d5e3fc2f8c93dffd232
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-17 17:42:03 +0530

    Update Data.js

--- plugins/Data.js
+++ plugins/Data.js
@@ -4,6 +4,6 @@ const Config = require('../config');
 
    bots.inrl({pattern: ['data'], desc: "to check whether", sucReact: "ðŸ¥¸", category: ['all'],},   async (message, client) => {
 if (!message.quoted) return await client.sendMessage( message.from, { text: 'please mension some one to take their data'}, { quoted: message });
-const url = `https://docs-jojo.herokuapp.com/api/fake_identity?lang=en&type=url`;
- await client.sendMessage(message.from,  { text : 'NAME  :' + url.name + '\n' + 'GENDER :'+ url.gender + 'AGE  :' + url.age + '\n' + 'BDAY :'+ url.birtday + 'JOB  :' + url.occupation + '\n' + 'ADDRESS :'+ url.address +'PIN  :' + url.zip_code + '\n' + 'PLACE :'+ url.state + 'COUNTRY  :' + url.country + '\n' + 'PHONE :'+ url.phone}, { quoted: message });
+const ibotmd = `https://docs-jojo.herokuapp.com/api/fake_identity?lang=en&type=ibotmd`;
+ return await client.sendMessage(message.from,  { text : 'NAME  :' + ibotmd.name + '\n' + 'GENDER :'+ ibotmd.gender + 'AGE  :' + ibotmd.age + '\n' + 'BDAY :'+ ibotmd.birtday + 'JOB  :' + ibotmd.occupation + '\n' + 'ADDRESS :'+ ibotmd.address +'PIN  :' + ibotmd.zip_code + '\n' + 'PLACE :'+ ibotmd.state + 'COUNTRY  :' + ibotmd.country + '\n' + 'PHONE :'+ ibotmd.phone}, { quoted: message });
 });
