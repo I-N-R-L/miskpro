3a60b6a6fbf0b74389011d14c50a3337aa07d278
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-17 15:18:15 +0530

    Update Sp.js

--- plugins/Sp.js
+++ plugins/Sp.js
@@ -13,12 +13,6 @@ const Config = require('../config');
 
 
     bots.inrl({pattern: ['sp'], desc: "to check whether", sucReact: "💔", category: ['all'],},   async (message, client) => {
-	    const url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥵_🥲`;
-	    try {
-		    const response = await got(url);
-		    const json = JSON.parse(response.body);
-		    if (response.statusCode === 200) return await client.sendMessage( message.from, { image: { url:  json.url }, caption: bots.config.exif.cap,},{ quoted: message });
-	    } catch {
-		    return await client.sendMessage( message.from, { text : "no data found on this location"},{ quoted: message });
-	    }
+	    const url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥵_😇`;
+		    return await client.sendMessage( message.from, { image: { url:  url.url }, caption: bots.config.exif.cap,},{ quoted: message });
     });
