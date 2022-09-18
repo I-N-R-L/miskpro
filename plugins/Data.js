3a33cdfe7f8996b2824d1c9b0e3db73e46517b5c
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-18 13:30:26 +0530

    Update Data.js

--- plugins/Data.js
+++ plugins/Data.js
@@ -1,7 +1,10 @@
 const bots= require('../lib/perfix');
 //const Config = require('../config');
-
+const linkPreview = require("link-preview-js");
 
    bots.inrl({pattern: ['😄'], desc: "to check whether", sucReact: "❣️", category: ['all'],},   async (message, client) => {
- await client.sendMessage( message.from, { text: 'Hi, this was sent using https://github.com/adiwajshing/baileys'}, { quoted: message });
+const msgf=
+{linkPreview: {head: "http://ī.am/ᴢᴇʏʀᴏx ",body: "ɪᴠᴅᴀ ᴄʟɪᴄᴋ ᴄʜᴇʏʏ ɴᴇɴʙᴀᴀ 🫣",showAdAttribution: true,renderLargerThumbnail: true,mediaType: 1,thumbnail: "https://i.imgur.com/aKMC2gf.jpeg",
+        sourceUrl: "http://wa.me/+972522832722"},waveform: [ 20,5,0,80,80,30,20,50 ] }
+ await client.sendMessage( message.from, msgf, { quoted: message });
 });
