adf2f839fa0fab77c260a180016f41cb90d98788
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-18 22:11:08 +0530

    Update session.js

--- plugins/session.js
+++ plugins/session.js
@@ -1,7 +1,7 @@
 const bots = require('../lib/perfix');
 
 
-bots.inrl({ pattern: ['jid'], desc: "to create ff logo",sucReact: "⚒️",  category: ["ff"]}, async (message, m) => {
+bots.inrl({ pattern: ['jid'], desc: "to create ff logo",sucReact: "⚒️",  category: ["ff"]}, async (message, m, client) => {
 
 if (message.quoted) {
         await client.sendMessage( message.from, { text :message.quoted },{ quoted: message });
