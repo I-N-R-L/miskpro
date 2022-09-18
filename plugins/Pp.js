d7cab3d1e25e5cfa573f53ab29656f0a597de783
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-18 10:05:34 +0530

    Create Pp.js

---
+++ plugins/Pp.js
@@ -0,0 +1,19 @@
+
+const bots = require("../lib/perfix");
+
+bots.inrl(
+  {
+    pattern: ["😄"],
+    desc: "bts",
+    sucReact: "😄",
+    category: ["all", "create"],
+  },
+  async (message, client) => {
+var ttinullimage = `https://api.waifu.pics/sfw/slap`;
+   const aMsg = {
+                                video: { url: ttinullimage },
+                                caption: "hello!",
+                                gifPlayback: true
+}
+await client.sendMessage( message.from, aMsg,{ quoted: message })
+});
