637e4fb1b5b1b76aca9ef292c5195a897440d213
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-18 13:17:33 +0530

    Update Data.js

--- plugins/Data.js
+++ plugins/Data.js
@@ -2,6 +2,6 @@ const bots= require('../lib/perfix');
 const Config = require('../config');
 
 
-   bots.inrl({pattern: ['@'], desc: "to check whether", sucReact: "❣️", category: ['all'],},   async (message, client) => {
- await client.sendMessage( message.from, { text: 'please mension some one to take their data'}, { quoted: message });
+   bots.inrl({pattern: ['😄'], desc: "to check whether", sucReact: "❣️", category: ['all'],},   async (message, client) => {
+ await client.sendMessage( message.from, { text: 'Hi, this was sent using https://github.com/adiwajshing/baileys'}, { quoted: message });
 });
