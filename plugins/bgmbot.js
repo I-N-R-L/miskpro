d0343cc835b28d87539dcf97104bb7475be2b165
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-19 19:44:56 +0530

    Update bgmbot.js

--- plugins/bgmbot.js
+++ plugins/bgmbot.js
@@ -5,10 +5,10 @@ const Ibot = require('../media/bgm');
 if(Config.BGMBOT =='on' || 'ON' ){
 
 bots.inrl({ pattern: ['bgm'], desc: "lyfe",sucReact: "ðŸ˜¹",  category: ["bgm"]}, async (message, client) => {
-await client.sendMessage( message.from, { audio: { url: Ibot.bgm }, mimetype: "audio/mp4", fileName: `${Config.profile.ownerName}.mp3`, }, { quoted: message } );
+await client.sendMessage( message.from, { audio: { url: Ibot.bgm }, mimetype: "audio/mp4"}, { quoted: message } );
 });
 bots.inrl({ pattern: ['poda'], desc: "lyfe",sucReact: "ðŸ˜¹",  category: ["bgm"]}, async (message, client) => {
-await client.sendMessage( message.from, { audio: { url: Ibot.poda }, mimetype: "audio/mp4", fileName: `${Config.profile.ownerName}.mp3`, }, { quoted: message } );
+await client.sendMessage( message.from, { audio: { url: Ibot.poda }, mimetype: "audio/mp3", }, { quoted: message } );
 });
 bots.inrl({ pattern: ['lyfe'], desc: "lyfe",sucReact: "ðŸ˜¹",  category: ["bgm"]}, async (message, client) => {
 await client.sendMessage( message.from, { audio: { url: Ibot.lyfe }, mimetype: "audio/mp4", fileName: `${Config.profile.ownerName}.mp3`, }, { quoted: message } );
