cc82e8fd86e7919f3699300cc12f444ce65670e8
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-21 02:56:52 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -8,17 +8,37 @@ bots.inrl(
     category: ["system", "all"],
   },
   async (message, client) => {
-const Message = {
-      image: { url: bots.config.image.url.D_E_TMB },
-      caption:`╭───────────────────
-┃☞︎︎︎│ *ɪᴀᴍ ᴀʟɪᴠᴇ ʙʀᴏ*
-┃∞│ ─━━━━━━━━━━━━━─
-┃☞︎︎︎│ *➪ ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ* 
-┃ɢɪᴛ :`+Config.GIT+`
-┃ꜱᴜᴩᴩᴏʀᴛ ɢʀᴩ: `+Config.WAGRP+`
-┃ᴡᴇʙ :`+Config.WEB+`
-┃∞│ ─━━━━━━━━━━━━━─
-╰───────────────────`
-    };
-    await client.sendMessage(message.from, Message, { quoted: message });
+const templateButtons = [
+    {index: 1, urlButton: {displayText: 'ɪɴꜱᴛᴀɢʀᴀᴍ', url: Config.INSTAGRAM}},
+]
+
+const templateMessage = {
+    image: { url: Config.ALIVE },
+    caption: `╭═══〘`+Config.profile.botName +`〙═══⊷❍
+┃☯︎╭──────────────
+┃☯︎│
+┃☯︎│ ᴏᴡɴᴇʀ :`+Config.profile.ownerName+`
+┃☯︎│ ᴜꜱᴇʀ : ${message.client.pushName}
+┃☯︎│ ᴍᴏᴅᴇ : `+ Config.WORKTYPE +`
+┃☯︎│ ꜱᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ
+┃☯︎│ ᴛᴏᴛᴇʟ ʀᴀᴍ : 16.93 
+┃☯︎│ ᴀᴠᴀʟɪʙʟᴇ ʀᴀᴍ : 0.95
+┃☯︎│ ᴛᴏᴛᴇʟ ꜱᴩᴀᴄᴇ : 512ᴍʙ
+┃☯︎│ ᴠᴇʀꜱɪᴏɴ : ${Config.VERSION}
+┃☯︎│ ɢɪᴛʜᴜʙ :`+Config.GIT+`
+┃☯︎│ ᴡᴇʙꜱɪᴛᴇ :`+Config.WEB+`
+┃☯︎│ ᴅᴀᴛᴀʙᴀꜱᴇ : ɪ-ʙᴏᴛ-ꜱᴇʀᴠᴇʀ
+┃☯︎│ ᴛᴜʀᴛᴏʀɪᴀʟ :`+Config.VIDEO+`
+┃☯︎│ yᴏᴜᴛᴜʙᴇ :`+Config.YT+`
+┃☯︎│
+┃☯︎│  --------------------
+┃☯︎│      `+Config.PACKNAME+`  
+┃☯︎│  --------------------
+┃☯︎│ 
+┃☯︎╰───────────────
+╰═════════════════⊷`
+    footer: Config.FOOTER,
+    templateButtons: templateButtons
+}
+await client.sendMessage(message.from, templateMessage, { quoted: message });
 });
