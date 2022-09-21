9da2f9574d05af2b18cba3ae5c6ab28d1911b0cc
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-21 15:18:13 +0530

    Update linkpr.js

--- plugins/linkpr.js
+++ plugins/linkpr.js
@@ -1,12 +1,5 @@
-/* ═══════════════════════════════════════════════════════ //
-=> If you want to recode, reupload,
-=> or copy the codes/script,
-=> pls give credit,
-=> no credit? i will take action immediately.
-==> Copyright (C) 2022 Dark_Ezio.
-==> Licensed under the  MIT License;
-===> you may not use this file except in compliance with the License.
-=> Thank you to Lord Buddha, Family and Myself.
+/*
+, Family and Myself.
 => Whats Bot - Dark_Ezio.
 // ════════════════════════════ */
 
@@ -39,3 +32,4 @@ ezio.inrl(
         quoted: ezio.config.quoted.product,
       }
     );
+ });
