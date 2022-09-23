ddd69752d9f1cdc01f4199659a948c63f747516e
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-23 13:41:33 +0530

    Create tts.js

---
+++ plugins/tts.js
@@ -0,0 +1,35 @@
+const bots = require('../lib/perfix');
+const googleTTS = require('google-translate-tts');
+
+bots.inrl({pattern: ['tts'], desc: "to check whether", sucReact: "💔", category: ['all'], }, (async (message, client) => {
+
+const text = message.client.text;
+	    if (!text) return await client.sendMessage( message.from, { text: 'Enter A text'}, { quoted: message });
+
+            let 
+                LANG = 'en',
+                ttsMessage = text,
+                SPEED = 1.0
+    
+            var buffer = await googleTTS.synthesize({
+                text: ttsMessage,
+                voice: LANG
+            });
+            await client.sendMessage( message.from, { audio: { url: buffer }, mimetype: "audio/mp4",ptt: true}, { quoted: message } );
+        }));
+bots.inrl({pattern: ['ttts'], desc: "to check whether", sucReact: "💔", category: ['all'], }, (async (message, client) => {
+
+const text = message.client.text;
+	    if (!text) return await client.sendMessage( message.from, { text: 'Enter A text'}, { quoted: message });
+
+            let 
+                LANG = 'en',
+                ttsMessage = text,
+                SPEED = 1.0
+    
+            var buffer = await googleTTS.synthesize({
+                text: ttsMessage,
+                voice: LANG
+            });
+            await client.sendMessage( message.from, { audio:buffer, mimetype: "audio/mp4",ptt: true}, { quoted: message } );
+        }));
