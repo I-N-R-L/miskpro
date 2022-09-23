db34b581a81a8514dc5d56723424feca0bdb7dd4
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-23 12:22:00 +0530

    Update Vot.js

--- plugins/Vot.js
+++ plugins/Vot.js
@@ -38,6 +38,111 @@ const button = {
 await client.sendMessage( message.from, button, { quoted: message, });
   
   }));
+const yts = require("yt-search");
+const lang = bots.getString("scrapers");
+
+bots.inrl(
+  { 
+    pattern: ["play", "ytplay","mp4"], 
+    desc: "you can dowloade audio from youtube", 
+    usage: '<url|query>',
+    sucReact: "🔎", 
+    category: ["search"] 
+},
+  async (message, client) => {
+    try {
+      if (!message.client.text) { await client.sendErrorMessage( message.from, lang.NEED_TEXT_SONG, message.key, message ); return global.catchError = true; }
+      let video = {};
+      let results = {};
+      let result;
+      let buttons = [];
+      if (message.client.args[0] == "x/65v79") {
+        video = await yts({ videoId: message.client.args[1] });
+        result = video;
+        buttons = [
+          { buttonId: `.mp3 ${result.url}`, buttonText: { displayText: "ᴀᴜᴅɪᴏ" }, type: 1, },
+          { buttonId: `.mp4 ${result.url}`, buttonText: { displayText: "ᴠɪᴅᴇᴏ" }, type: 1, },
+        ];
+      } else {
+        results = await yts(message.client.text);
+        result = results.videos[0];
+        buttons = [
+          { buttonId: `.mp3 ${result.url}`, buttonText: { displayText: "ᴀᴜᴅɪᴏ" }, type: 1, },
+          { buttonId: `.mp4 ${result.url}`, buttonText: { displayText: "ᴠᴇᴅɪᴏ" }, type: 1, },
+          { buttonId: `.play ${message.client.text}`, buttonText: { displayText: "ꜱᴇᴀʀᴄʜ" }, type: 1, },
+        ];
+      }
+      let Message = {
+        image: { url: result.thumbnail },
+        caption: `
+  —————————————————————
+                        ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ 
+  —————————————————————
+  ♻ ᴛɪᴛɪʟᴇ : ${result.title}
+  ♻ ᴏᴜᴛᴩᴜᴛ : Search [first result]
+  ♻ ᴜʀʟ-ɪᴅ : ${result.videoId}
+  ♻ ʟᴇɴɢᴛʜ: ${result.timestamp}
+  ♻ ᴠɪᴡᴇʀꜱ : ${result.views}
+  ♻ ᴜᴩʟᴏᴀᴅᴇᴅ: ${result.ago}
+  ♻ ᴀᴜᴛʜᴇʀ : ${result.author.name}
+  ♻ ᴄʜᴀɴɴᴇʟ : ${result.author.url}
+  ♻ ᴅɪꜱᴄʀɪᴩᴛɪᴏɴ : ${result.description}
+  ♻ ᴜʀʟ : ${result.url}
+  —————————————————————————`,
+        footer: bots.config.exif.footer,
+        buttons: buttons,
+      };
+
+      await client.sendMessage(message.from, Message, { quoted: message});
+    } catch (error) { global.catchError = true; return await client.sendErrorMessage( message.from, error, message.key, message ); }
+  }
+);
+
+bots.inrl(
+  { 
+    pattern: ["mp4", "play"], 
+    desc: "you can dowloade Randomly yt result downlode", 
+    usage: '<url|query>',
+    sucReact: "🔎", 
+    category: ["search", "all"] 
+},
+  async (message, client) => {
+    try {
+      if (!message.client.text) { await client.sendErrorMessage( message.from, lang.NEED_TEXT_SONG, message.key, message ); return global.catchError = true; }
+      const results = await yts(message.client.text);
+      let result = results.videos[Math.floor(Math.random() * results.videos.length)];
+      let buttons = [
+        { buttonId: `.mp3 ${result.url}`, buttonText: { displayText: "🎼 Audio 🎵" }, type: 1, },
+        { buttonId: `.mp4 ${result.url}`, buttonText: { displayText: "🎞 Video 📽️" }, type: 1, },
+        { buttonId: `.play ${message.client.text}`, buttonText: { displayText: "🔎 Random Search 🔍" }, type: 1, },
+      ];
+      let buttonMessage = {
+        image: { url: result.thumbnail },
+        caption: `
+  —————————————————————————
+  ♻ ᴛɪᴛɪʟᴇ: ${result.title}
+  ♻ ᴏᴜᴛᴩᴜᴛ: Search [Random result]
+  ♻ ᴜʀʟ-ɪᴅ: ${result.videoId}
+  ♻ ꜱɪᴢᴇ : ${result.timestamp}
+  ♻ ᴠɪᴡᴇʀꜱ : ${result.views}
+  ♻ ᴜᴩʟᴏᴀᴅᴇᴅ : ${result.ago}
+  ♻ ᴀᴜᴛʜᴏʀ : ${result.author.name}
+  ♻ ᴄʜᴀɴɴᴇʟ : ${result.author.url}
+  ♻ ᴅɪꜱᴄʀᴩᴛɪᴏɴ : ${result.description}
+  ♻ ᴜʀʟ : ${result.url}
+  —————————————————————————`,
+        footer: bots.config.exif.footer,
+        buttons: buttons,
+      };
+      await client.sendMessage(message.from, buttonMessage, { quoted: message, });
+      global.catchError = false;
+      } catch (error) { 
+        global.catchError = true; 
+        return await client.sendErrorMessage( message.from, error, message.key, message ); 
+      }
+  }
+);
+
 bots.inrl(
   {
     pattern: ["play", "song", "mp3"],
