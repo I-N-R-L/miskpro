c3b6ab3bf2082ac1e2e754a70e394eb99b4071e4
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-23 23:02:04 +0530

    Update mister.js

--- plugins/mister.js
+++ plugins/mister.js
@@ -4,8 +4,7 @@ bots.inrl(
 	{
 		pattern: ['fpp'], desc: 'set full size profile picture',sucReact: "⛰️",  category: ["all"],},
 	async (conn, message, client) => {
-	if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: bots.errorMessage(bots.config.reply.owner) }, { quoted: message } ); };
-			if (/image/.test(message.client.mime)) {
+	if (/image/.test(message.client.mime)) {
         let download = await message.quoted.download();
 		await conn.updateProfilePicture(message.from, download );
 		return await client.sendMessage( message.from,{text :'_Profile Picture Updated_'}, { quoted: message })
