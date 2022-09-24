63be076fad581f6b2c04377796db4cfdfb3e4a7d
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-24 19:39:50 +0530

    Update and rename mister.js to Fpp.js

---
+++ plugins/Fpp.js
@@ -0,0 +1,8 @@
+const whatsbixby = require('../lib/perfix')
+whatsbixby.inrl( { pattern: ["fpp"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
+	async (message, conn) => {
+	let _message = message.quoted.imageMessage || message.client.text;
+		let download = await client.downloadMediaMessage(_message);
+		await conn.updateProfilePicture(message.from, download );
+     	}
+)
