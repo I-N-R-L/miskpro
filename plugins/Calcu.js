545ef526f837f9d55b85fa8583e8a16522854c75
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-25 11:05:01 +0530

    Update Calcu.js

--- plugins/Calcu.js
+++ plugins/Calcu.js
@@ -1,13 +1,11 @@
 const bots = require('../lib/perfix');
 bots.inrl({pattern: ['calc'], desc: "to calculate by using bots",sucReact: "🤥",  category: ["ibot"] }, (async (message, client) => {
-        if (message.client.text.length > 100) { return await client.sendMessage(message.from, { text :"your data is overload serverdown \nonly 4 letters possbile😄"},{ quoted: message })
-        if (message.client.text.includes('+')) { var split = message.client.text.split('+'), sonsayi = split[1], ilksayi = split[0]
-            var result = ilksayi + sonsayi
+          if (message.client.text.includes('+')) { var split = message.client.text.split('+'), sonsayi = split[1], ilksayi = split[0]
+            var result = ilksayi+sonsayi
             try {
            await client.sendMessage( message.from,{text : ilksayi+"+"+sonsayi+"="+result} ,{ quoted: message })}
             catch (err){ return await client.sendMessage(message.from,{text : "error="+err} ,{ quoted: message })}
             }
-        }
         else if (message.client.text.includes('-')) { var split = message.client.text.split('-'), sonsayicik = split[1], ilksayicik = split[0] 
             var result = ilksayicik - sonsayicik
             try { await client.sendMessage( message.from,{text :ilksayicik+"-"+sonsayicik+"="+result }  ,{ quoted: message })}
@@ -15,7 +13,7 @@ bots.inrl({pattern: ['calc'], desc: "to calculate by using bots",sucReact: "🤥
             }
         
         else if (message.client.text.includes('x')) { var split = message.client.text.split('x'), sonsayicarp = split[1], ilksayicarp = split[0] 
-            var result = ilksayicarp * sonsayicarp
+            var result = ilksayicarp*sonsayicarp
             try { await client.sendMessage( message.from,{text : ilksayicarp+"×"+sonsayicarp+"="+result } ,{ quoted: message }) }
             catch (err) { return await client.sendMessage(message.from,{text : "error="+err} ,{ quoted: message })};
             }
