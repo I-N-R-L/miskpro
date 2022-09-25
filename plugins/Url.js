aa764cbd11f6ac22137716ecd9549a849b802eaf
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-25 15:44:52 +0530

    Update Url.js

--- plugins/Url.js
+++ plugins/Url.js
@@ -1,6 +1,6 @@
 const imgbbUploader = require("imgbb-uploader");
 const bots = require('../lib/perfix')
-const api = ["76a050f031972d9f27e329d767dd988f","deb80cd12ababea1c9b9a8ad6ce3fab2","78c84c62b32a88e86daf87dd509a657a"];
+const api = "76a050f031972d9f27e329d767dd988f" || "deb80cd12ababea1c9b9a8ad6ce3fab2";
 bots.inrl(
 	{
 		pattern: ['urli'],
