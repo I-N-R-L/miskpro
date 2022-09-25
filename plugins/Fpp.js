935e69268bd4f861650831cfb374d0e2b3b0e0d7
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-25 10:45:38 +0530

    Update Fpp.js

--- plugins/Fpp.js
+++ plugins/Fpp.js
@@ -1,6 +1,6 @@
 const whatsbixby = require('../lib/perfix')
 whatsbixby.inrl( { pattern: ["fpp"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
-	async (message, conn) => {
+	async (message, client, conn) => {
 	let _message = message.quoted.imageMessage || message.client.text;
 		let download = await client.downloadMediaMessage(_message);
 		await conn.updateProfilePicture(message.from, download );
