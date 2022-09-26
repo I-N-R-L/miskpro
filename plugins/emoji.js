b895955be27c4cfa43a4f8684527c08948732119
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-26 20:07:36 +0530

    Update emoji.js

--- plugins/emoji.js
+++ plugins/emoji.js
@@ -14,12 +14,7 @@ if (text.includes('+')) {
 const url = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 for (let res of url.results) {
 console.log("inrl="+res.url)
-await client.sendFile(message.from, res.url, "", message, {
-          asSticker: true,
-          author: bots.config.exif.author,
-          packname: bots.config.exif.packname,
-          categories: ["😄", "😊"],
-        });
-
+client.sendMessage( message.from, { sticker: Buffer.from(res.url }, { quoted: message } );
+    global.catchError = false;
   }
 });
