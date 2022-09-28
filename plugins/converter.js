ed4050e5b1a9a48820a512cf2b2b7cd0b375464a
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-28 13:38:11 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -14,7 +14,7 @@ let { toPTT } = require('../lib/converter')
 bots.inrl({pattern: ['tiktok'], desc: "to downlode tiktok video",sucReact: "🌇",  category: ["all"]}, async (message, client) => {
 const text = message.client.text;
 if (!text) return await client.sendMessage(message.from, { text :' enter a tiktok link '},{ quoted: message })
-if (!isUrl(args[0]) && !args[0].includes('tiktok.com')) return await client.sendMessage(message.from, { text :' enterd a tiktok link is not valid'},{ quoted: message })
+if (!isUrl(message.client.args[0]) && !message.client.args[0].includes('tiktok.com')) return await client.sendMessage(message.from, { text :' enterd a tiktok link is not valid'},{ quoted: message })
 url = await fetchJson(`https://violetics.pw/api/downloader/tiktok?apikey=df7d-425a-3bc8&url=${text}`)
 let listmn = `ᴛɪᴋᴛᴏᴋ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ\n\n*ᴛɪᴛɪʟᴇ:* ${url.result.title}\n\ᴀᴜᴛʜᴇʀ:${url.result.id}\n\nᴜʀʟ: ${url.result.url}`
 buf = await getBuffer(url.result.thumb)
@@ -33,7 +33,7 @@ await client.sendMessage(message.from, onMessage, { quoted: message});
 bots.inrl({pattern: ['ig'], desc: "to download istagram video",sucReact: "🌇",  category: ["all"]}, async (message, client) => {
 const text = message.client.text;     
 if (!text) return await client.sendMessage(message.from, { text :`enter a instagram link _ex_:${Config.INSTAGRAM}`},{ quoted: message })
-if (!isUrl(args[0]) && !args[0].includes('instagram.com')) return await client.sendMessage(message.from, { text :'entered instagram link is not valid'},{ quoted: message })
+if (!isUrl(message.client.args[0]) && !message.client.args[0].includes('instagram.com')) return await client.sendMessage(message.from, { text :'entered instagram link is not valid'},{ quoted: message })
    
 instagramdlv3(`${text}`).then(async (data) => {
 for (let f of data) {                                      
@@ -46,7 +46,7 @@ await client.sendMessage( message.from, { video: { url: f.url }, mimetype: "vide
 bots.inrl({ pattern: ['fbmp3'], desc: "to downlode fb mp3",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
 const text = message.client.text;
 if (!text) return await client.sendMessage(message.from, { text :"enter a fb link"},{ quoted: message })
-if (!isUrl(args[0]) && !args[0].includes('facebook.com')) { global.catchError = true; }
+if (!isUrl(message.client.args[0]) && !message.client.args[0].includes('facebook.com')) { global.catchError = true; }
      
 noh.savefrom(`${text}`).then(async (inrl) => { 
 await client.sendMessage( message.from, { audio: { url: inrl.url[0].url }, mimetype: "audio/mpeg", fileName: `${Config.FREE_TXT}.mp3`, }, { quoted: message } );
@@ -56,7 +56,7 @@ client.sendMessage(message.from, { text :"filed to download"},{ quoted: message 
 });
 bots.inrl({ pattern: ['photo','toimg'], desc: "to convert webp to img",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
    if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
-   if (!/webp/.test(mime)) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
+   if (!/webp/.test(message.client.mime)) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
 let _message = message.quoted.stickerMessage ;
    let media = await client.downloadAndSaveMediaMessage(_message)
    let ran = await getRandom('.png')
@@ -70,7 +70,7 @@ let _message = message.quoted.stickerMessage ;
  });
  bots.inrl({ pattern: ['video','tomp4'], desc: "to convert webp to mp4",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
    if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
-   if (!/webp/.test(mime)) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
+   if (!/webp/.test(message.client.mime)) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
 let _message = message.quoted.stickerMessage ;
    let media = await client.downloadAndSaveMediaMessage(_message)
    let webpToMp4 = await webp2mp4File(media)
@@ -78,7 +78,7 @@ let _message = message.quoted.stickerMessage ;
    await fs.unlinkSync(media)
  });
  bots.inrl({ pattern: ['mp3','toaudio'], desc: "to convert video to mp3",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
- if (!/video/.test(mime) && !/audio/.test(mime)) return await client.sendMessage(message.from, { text : "Send/Reply Video/Audio You Want Audio With Caption" },{ quoted: message })
+ if (!/video/.test(message.client.mime) && !/audio/.test(message.client.mime)) return await client.sendMessage(message.from, { text : "Send/Reply Video/Audio You Want Audio With Caption" },{ quoted: message })
  if (!message.quoted) return await client.sendMessage(message.from, { text :"Send/Reply Video/Audio You Want to Use as Audio With Caption " },{ quoted: message })
 let _message = message.quoted.videoMessage || message.quoted.audioMessage;
    let media = await client.downloadAndSaveMediaMessage(_message)
@@ -86,7 +86,7 @@ let _message = message.quoted.videoMessage || message.quoted.audioMessage;
 await client.sendMessage( message.from, { audio: { url: audio }, mimetype: "audio/mpeg", fileName: `${Config.FREE_TXT}.mp3`, }, { quoted: message } );
  });
 bots.inrl({ pattern: ['voice','ptt'], desc: "to convert audio/video to ptt",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
- if (!/video/.test(mime) && !/audio/.test(mime)) return await client.sendMessage(message.from, { text : "Reply Video/Audio That You Want To Be VN With Caption " },{ quoted: message });
+ if (!/video/.test(message.client.mime) && !/audio/.test(message.client.mime)) return await client.sendMessage(message.from, { text : "Reply Video/Audio That You Want To Be VN With Caption " },{ quoted: message });
  if (!message.quoted) return await client.sendMessage(message.from, { text :"Reply Video/Audio That You Want To Be VN With Caption " },{ quoted: message });
  let _message = message.quoted.videoMessage || message.quoted.audioMessage;
    let media = await client.downloadAndSaveMediaMessage(_message)
@@ -95,7 +95,7 @@ await client.sendMessage( message.from, { audio: { url: audio }, mimetype: "audi
  });
  bots.inrl({ pattern: ['togif'], desc: "to convert webp to gif",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
    if (!message.quoted) return await client.sendMessage(message.from, { text : "Reply An img " },{ quoted: message });
-   if (!/webp/.test(mime)) return await client.sendMessage(message.from, { text : "this features is used to convert webp to gif playback" },{ quoted: message });
+   if (!/webp/.test(message.client.mime)) return await client.sendMessage(message.from, { text : "this features is used to convert webp to gif playback" },{ quoted: message });
   let _message = message.quoted.stickerMessage ;
    let media = await client.downloadAndSaveMediaMessage(_message)
    let webpToMp4 = await webp2mp4File(media)
