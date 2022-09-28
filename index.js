808e1f61555a3f69a504823aae3133f9853c8395
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-28 07:49:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,6 +82,9 @@ console.log(' session file cretion failed ');
     await chatting(m, conn);
     await circle(m, conn);
     await faketest(m, conn);
+   setInterval(async () => {
+    await conn.sendMessage(conn.user.id, {text :`inrl-bot-md workignow💗\n\nMODE : ${Config.WORKTYPE}\n\n`});
+   }, 10000 * 1000);
     try {
      ezio.commands.map(async (command) => {
         for (let i in command.pattern) {
@@ -110,9 +113,6 @@ if(Config.U_STATUS =='true'){
   }, 1000 * 10);
   if (conn.user && conn.user?.id) conn.user.jid = jidNormalizedUser(conn.user?.id); conn.logger = conn.type == "legacy" ? DEFAULT_LEGACY_CONNECTION_CONFIG.logger.child({}) : DEFAULT_CONNECTION_CONFIG.logger.child({});
              };
-setInterval(async () => {
-    await conn.sendMessage(conn.user.id, {text :`inrl-bot-md workignow💗\n\nMODE : ${Config.WORKTYPE}\n\n`});
-   }, 10000 * 1000);
          });
      };
 WhatsBotConnect();
