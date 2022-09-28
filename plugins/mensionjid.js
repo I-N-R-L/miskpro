5ee52f286b0e354739c3a8c56fb83c7acbe69805
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-28 18:41:37 +0530

    Update mensionjid.js

--- plugins/mensionjid.js
+++ plugins/mensionjid.js
@@ -1,9 +1,6 @@
 const bots = require("../lib/perfix");
 const fs = require('fs');
-let global.db = JSON.parse(fs.readFileSync('./lib/database/server.json'));
-if (global.db) global.db = {
-    sticker: {},
-}
+let sticker = JSON.parse(fs.readFileSync('./lib/database/server.json'));
 
 bots.inrl({ pattern: ["setcmd"], usage: '<mentions|reply>', sucReact: "😎", category: ["group", "all"],},
   async (message, client) => {
@@ -11,9 +8,9 @@ bots.inrl({ pattern: ["setcmd"], usage: '<mentions|reply>', sucReact: "😎", ca
    if (!message.quoted) { global.catchError = true; return await client.sendMessage( message.from, { text: bots.errorMessage(bots.config.reply.owner) }, { quoted: message } ); };
    if (!message.quoted.fileSha256) {return await client.sendMessage(message.from, { text: 'SHA256 Hash Missing'},{ quoted: message } ); }
    if (!text) {return await client.sendMessage(message.from, { text: 'where is the cmd'},{ quoted: message } ); }
-   let hash = m.quoted.fileSha256.toString('base64')
-   if (global.db.sticker[hash] && global.db.sticker[hash].locked) {return await client.sendMessage(message.from, { text: 'You have no permission to change this sticker command'}, { quoted: message } );
-   global.db.sticker[hash] = {
+   let hash = message.quoted.fileSha256.toString('base64')
+   if (sticker[hash] && sticker[hash].locked) {return await client.sendMessage(message.from, { text: 'You have no permission to change this sticker command'}, { quoted: message } );
+   sticker[hash] = {
   text,
   mentionedJid: message.mentionedJid,
   creator: message.sender,
