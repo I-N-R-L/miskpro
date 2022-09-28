fe8520683eade54b5ee194a4617fc51cf64ec9ce
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-28 07:22:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -112,3 +112,10 @@ if(Config.U_STATUS =='true'){
          });
      };
 WhatsBotConnect();
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update ${__filename}`)
+	delete require.cache[file]
+	require(file)
+})
