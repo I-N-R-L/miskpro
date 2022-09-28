a1530b113f81cb714a578a1837f275a1fb153c5d
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-28 19:06:53 +0530

    Update mensionjid.js

--- plugins/mensionjid.js
+++ plugins/mensionjid.js
@@ -1,9 +1,25 @@
 const bots = require("../lib/perfix");
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, WAZimBotIncection, MessageType, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType, fetchLatestBaileysVersion } = require('@adiwajshing/baileys')
 const fs = require('fs');
 let sticker = JSON.parse(fs.readFileSync('./lib/database/server.json'));
 
 bots.inrl({ pattern: ["setcmd"], usage: '<mentions|reply>', sucReact: "😎", category: ["group", "all"],},
   async (message, client) => {
+if (message.client.isMedia && message.msg.fileSha256 && (message.msg.fileSha256.toString('base64') in sticker)) {
+let hash = sticker[message.msg.fileSha256.toString('base64')]
+let { text, mentionedJid } = hash
+let messages = await generateWAMessage(message.chat, { text: text, mentions: mentionedJid }, {
+ quoted: message.quoted && message.quoted.fakeObj
+})
+messages.key.fromMe = areJidsSameUser(message.sender, client.user.id)
+messages.key.id = message.key.id
+messages.pushName = message.client.pushName
+if (message.client.isGroup) messages.participant = message.sender
+let msg = {
+ ...chatUpdate,
+ messages: [proto.WebMessageInfo.fromObject(messages)],
+ type: 'append'
+}
    const text = message.client.text;
    if (!message.quoted) { global.catchError = true; return await client.sendMessage( message.from, { text: bots.errorMessage(bots.config.reply.owner) }, { quoted: message } ); };
    if (!message.quoted.fileSha256) {return await client.sendMessage(message.from, { text: 'SHA256 Hash Missing'},{ quoted: message } ); }
