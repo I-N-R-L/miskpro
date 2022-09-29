eda31938c7953112c7ee7017998f63512bc7f95f
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 09:26:24 +0530

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 bots.inrl({ pattern: ["add"], usage: '<num1/numb2&etc>', sucReact: "😋", category: ["group", "all"], },
   async (message, client) => {
 const text = message.client.text;
-    if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: bots..errorMessage(bots.config.reply.owner) }, { quoted: message } ); };
+    if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: bots.errorMessage(bots.config.reply.owner) }, { quoted: message } ); };
     if (!message.isGroup) { global.catchError = true; return await client.sendMessage( message.from, { text: bots.errorMessage(bots.config.reply.group) }, { quoted: message } ); }
 let users = message.quoted ? message.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
         await client.sendMessage( message.from, { text: ezio.infoMessage("😋 Add group member. Using number.") }, { quoted: message } );
