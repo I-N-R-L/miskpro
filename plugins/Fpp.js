48047e1dda0190d2c3c8719a1731e895cdc5dac9
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 06:49:57 +0530

    Update Fpp.js

--- plugins/Fpp.js
+++ plugins/Fpp.js
@@ -3,6 +3,18 @@ whatsbixby.inrl( { pattern: ["fpp"],desc: 'set full size profile picture', sucRe
 	async (message, client, conn) => {
 	let _message = message.quoted.imageMessage || message.client.text;
 		let download = await client.downloadMediaMessage(_message);
-		await conn.updateProfilePicture(download);
+		//let users = message.quoted ? message.quoted.sender : message.displayText.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+        await client.updateProfilePicture(message.client.botNumber, { url: download }).catch((err) => fs.unlinkSync(download))
+      //await conn.updateProfilePicture(download);
      	}
-)
+);
+const whatsbixby = require('../lib/perfix')
+whatsbixby.inrl( { pattern: ["fppp"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
+	async (message, client, conn) => {
+	let _message = message.quoted.imageMessage || message.client.text;
+		let download = await client.downloadMediaMessage(_message);
+		//let users = message.quoted ? message.quoted.sender : message.displayText.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+        await client.updateProfilePicture(message.client.botNumber,download ).catch((err) => fs.unlinkSync(download))
+      //await conn.updateProfilePicture(download);
+     	}
+);
