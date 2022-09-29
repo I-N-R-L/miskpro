4232f64b7d88710429db0ddd58617100e30f63f1
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 09:24:14 +0530

    Update verix.js

--- plugins/verix.js
+++ plugins/verix.js
@@ -1,9 +1,9 @@
 const bots = require('../lib/perfix');
-//const verix = require('../media/virtex');
-/*
-bost.inrl( { pattern: ["virtex"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
+const verix = require('../media/virtex');
+
+bots.inrl( { pattern: ["virtex"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
 	async (message, client) => {
 await client.sendMessage( message.from,{text : verix.MENSION } ,{ quoted: message })
      	}
 );
-*/
+
