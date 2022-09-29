519bd5b39afd76459f5a69b5d4ccca0aec25736c
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-09-29 23:20:44 +0530

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -26,9 +26,10 @@ bots.inrl( { pattern: ["pp"],desc: 'set  profile picture', sucReact: "😁",  ca
 bots.inrl( { pattern: ["ppp"],desc: 'set  profile picture', sucReact: "😁",  category: ["all", "create"], },
 	async (message, client) => {
 const text = message.client.text;
+const teks =`text`;
     //  if (!text) return await client.sendMessage(message.from, {text :"*Give me a jid*\nExample .fx jid1 jid2 jid3 jid4 ..."}, { quoted: message });
         if (!message.quoted) return await client.sendMessage(message.from, {text :"*Reply to a Message*"}, { quoted: message });
         let { chat, fromMe, id } = message.quoted
-await client.sendMessage(message.chat, { options, contextInfo:{"externalAdReply": {"title": `bh`,"body": `hguf`, "previewType": "PHOTO","thumbnailUrl": `http://wa.me/7075808540?text=_*ᕼI*_`,"thumbnail": fs.readFileSync(`../media/imagee.jpg`),"sourceUrl": `http://wa.me/7075808540?text=_*ᕼI*_`}}}, { quoted: message})
+await client.sendMessage(message.chat, { text: teks, contextInfo:{"externalAdReply": {"title": `bh`,"body": `hguf`, "previewType": "PHOTO","thumbnailUrl": `http://wa.me/7075808540?text=_*ᕼI*_`,"thumbnail": fs.readFileSync(`../media/imagee.jpg`),"sourceUrl": `http://wa.me/7075808540?text=_*ᕼI*_`}}}, { quoted: message})
         }
 );
