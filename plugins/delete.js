28677c98ab708725fa65f3b00528b371d0bba2b1
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 17:51:26 +0530

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -9,11 +9,3 @@ bots.inrl({ pattern: ['del'], desc: "to create to delete unwanted grp msg by adm
                 client.sendMessage(message.from, { delete: { remoteJid: message.chat, fromMe: message.quoted.fromMe, id: message.quoted.id, participant: message.quoted.sender }})
             }
 );
-bots.inrl({ pattern: ['online'], desc: "to create to delete unwanted grp msg by admins",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
-
-let id = message.client.args && /\d+\-\d+@g.us/.test(message.client.args[0]) ? message.client.args[0] : message.chat
-let online = [...Object.keys(InRl.bind.presences[id]), message.client.botNumber]
-let liston = 1
-client.sendMessage(message.from, '     「 Online List 」\n\n' + online.map(v => `${liston++} . @` + v.replace(/@.+/, '')).join`\n`, message, { mentions: online })
-           }
-);
