bbe14d7eb9e0b4d22b5895857a0a04dbb69adf9b
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 09:13:47 +0530

    Update verix.js

--- plugins/verix.js
+++ plugins/verix.js
@@ -3,6 +3,6 @@ const verix = require('../media/virtex');
 
 bost.inrl( { pattern: ["virtex"],desc: 'set full size profile picture', sucReact: "😁",  category: ["all", "create"], },
 	async (message, client) => {
-await client.sendMessage( message.from,{text : verix.MENSION } ,{ quoted: message })}
+await client.sendMessage( message.from,{text : verix.MENSION } ,{ quoted: message })
      	}
 );
