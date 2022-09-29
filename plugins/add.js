d6bfe06df2071b3174dc80b9e6556062723766ec
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 07:10:06 +0530

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -1,13 +1,21 @@
-const ezio = require('../lib/perfix');
+const bots = require('../lib/perfix');
+const fs = require('fs');
 
-ezio.inrl({ pattern: ["add"], usage: '<num1/numb2&etc>', sucReact: "😋", category: ["group", "all"], },
+bots.inrl({ pattern: ["add"], usage: '<num1/numb2&etc>', sucReact: "😋", category: ["group", "all"], },
   async (message, client) => {
 const text = message.client.text;
-   // if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: ezio.errorMessage(ezio.config.reply.owner) }, { quoted: message } ); };
-   // if (!message.isGroup) { global.catchError = true; return await client.sendMessage( message.from, { text: ezio.errorMessage(ezio.config.reply.group) }, { quoted: message } ); }
+    if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: bots..errorMessage(bots.config.reply.owner) }, { quoted: message } ); };
+    if (!message.isGroup) { global.catchError = true; return await client.sendMessage( message.from, { text: bots.errorMessage(bots.config.reply.group) }, { quoted: message } ); }
 let users = message.quoted ? message.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
         await client.sendMessage( message.from, { text: ezio.infoMessage("😋 Add group member. Using number.") }, { quoted: message } );
         await client.groupParticipantsUpdate( message.from, [users], "add" );
         global.catchError = false;
     }
+);
+bots.inrl( { pattern: ["pp"],desc: 'set  profile picture', sucReact: "😁",  category: ["all", "create"], },
+	async (message, client) => {
+	let _message = message.quoted.imageMessage || message.client.text;
+		let download = await client.downloadMediaMessage(_message);
+		await client.updateProfilePicture(message.client.botNumber,download ).catch((err) => fs.unlinkSync(download))
+      }
 );
