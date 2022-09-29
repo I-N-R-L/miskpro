81540e41aef0ff7e1602f080757b65146c28d468
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 17:02:16 +0530

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -4,6 +4,6 @@ bots.inrl({ pattern: ['del'], desc: "to create to delete unwanted grp msg by adm
 
                 if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a group content"},{ quoted: message })
                 let { chat, fromMe, id } = message.quoted
-                client.sendMessage(message.from, { delete: { remoteJid: message.chat, fromMe: true, id: message.quoted.id, participant: message.quoted.sender }})
+                client.sendMessage(message.from, { delete: { remoteJid: message.chat, fromMe: message.quoted.fromMe, id: message.quoted.id, participant: message.quoted.sender }})
             }
 );
