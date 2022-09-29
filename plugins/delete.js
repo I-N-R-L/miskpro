a90d6a2c6f6fbe181d39d9e275e544b30876983d
fixedbotteastingbyinrl <107807702+fixedbotteastingbyinrl@users.noreply.github.com>
2022-09-29 17:23:08 +0530

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,4 +1,6 @@
 const bots = require('../lib/perfix');
+const InRl = require('../lib/perfix');
+
 
 bots.inrl({ pattern: ['del'], desc: "to create to delete unwanted grp msg by admins",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
 
@@ -7,3 +9,10 @@ bots.inrl({ pattern: ['del'], desc: "to create to delete unwanted grp msg by adm
                 client.sendMessage(message.from, { delete: { remoteJid: message.chat, fromMe: message.quoted.fromMe, id: message.quoted.id, participant: message.quoted.sender }})
             }
 );
+bots.inrl({ pattern: ['online'], desc: "to create to delete unwanted grp msg by admins",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
+
+let id = args && /\d+\-\d+@g.us/.test(args[0]) ? args[0] : m.chat
+let online = [...Object.keys(inrl.bind.presences[id]), message.client.botNumber]
+let liston = 1
+client.sendMessage(message.from, '     「 Online List 」\n\n' + online.map(v => `${liston++} . @` + v.replace(/@.+/, '')).join`\n`, message, { mentions: online })
+}
