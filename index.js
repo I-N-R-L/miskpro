b7cf131311db68d70bc7d2894f0ff175183f1ea7
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-30 21:51:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -85,9 +85,11 @@ console.log(' session file cretion failed ');
     try {
      ezio.commands.map(async (command) => {
         for (let i in command.pattern) {
-          if (command.pattern[i] == m.client.command || command.on == "text" && command.fromMe === true ? m.client.isCreator : m.from){
+          if (command.pattern[i] == m.client.command || command.on == "text"){
             global.isInCmd = true; global.mydb.hits += 1; global.catchError = false;
+if(Config.REACT =='true'){
             await conn.sendReact(m.from, await ezio.reactArry("INFO"), m.key);
+}
             await conn.sendPresenceUpdate( ezio.config.auto.presence.value, m.from );
             try {await command.function(m, conn);}
             catch (error) { global.catchError = true; console.log(error); }
