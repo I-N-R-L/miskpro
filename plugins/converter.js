43d8fdc4b231c2cac463a2b721cb3075d99ba0d0
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-01 15:06:40 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -64,7 +64,7 @@ let _message = message.quoted.stickerMessage ;
   fs.unlinkSync(media)
   if (err) client.sendMessage(message.from, { text: err }, { quoted: message });
   let buffer = fs.readFileSync(ran)
-  client.sendMessage(message.from, { image: { url: buffer }, caption: bots.config.exif.cap }, { quoted: message });
+  client.sendMessage(message.from, { image:  buffer , caption: bots.config.exif.cap }, { quoted: message });
   fs.unlinkSync(ran)
    })
  });
