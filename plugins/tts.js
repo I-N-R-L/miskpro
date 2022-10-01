60027b8e0ff83011c74107a905ea66707e9cd065
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-01 13:12:38 +0530

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -1,29 +1,17 @@
 const bots = require('../lib/perfix');
 const googleTTS = require('google-translate-tts');
 
-bots.inrl({pattern: ['tts'], desc: "to check whether", sucReact: "💔", category: ['all'], }, (async (message, client) => {
+bots.inrl({pattern: ['tts'], desc: "to get text as audio ", sucReact: "💔", category: ['all'], }, (async (message, client) => {
 
 const text = message.client.text;
 	    if (!text) return await client.sendMessage( message.from, { text: 'Enter A text'}, { quoted: message });
-
+            var InRL ;
+            if (text.includes('#')) {
+            var split = text.split('#');
+            InRL = split[1] || 'en';
+           }
             let 
-                LANG = 'en',
-                ttsMessage = text,
-                SPEED = 1.0
-    
-            var buffer = await googleTTS.synthesize({
-                text: ttsMessage,
-                voice: LANG
-            });
-            await client.sendMessage( message.from, { audio: { url: buffer }, mimetype: "audio/mp4",ptt: true}, { quoted: message } );
-        }));
-bots.inrl({pattern: ['ttts'], desc: "to check whether", sucReact: "💔", category: ['all'], }, (async (message, client) => {
-
-const text = message.client.text;
-	    if (!text) return await client.sendMessage( message.from, { text: 'Enter A text'}, { quoted: message });
-
-            let 
-                LANG = 'ml',
+                LANG = InRL,
                 ttsMessage = text,
                 SPEED = 1.0
     
