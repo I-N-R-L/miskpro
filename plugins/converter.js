45764e532c7e6cd2fb398384eea516412bdfaa95
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-01 16:24:52 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -80,9 +80,8 @@ let _message = message.quoted.stickerMessage ;
  bots.inrl({ pattern: ['mp3','toaudio'], desc: "to convert video to mp3",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
  if (!/video/.test(message.client.mime) && !/audio/.test(message.client.mime)) return await client.sendMessage(message.from, { text : "Send/Reply Video/Audio You Want Audio With Caption" },{ quoted: message })
  if (!message.quoted) return await client.sendMessage(message.from, { text :"Send/Reply Video/Audio You Want to Use as Audio With Caption " },{ quoted: message })
-let _message = message.quoted.videoMessage;
-   let media = await client.downloadAndSaveMediaMessage(_message);
- let audio = await toAudio(media, 'mp3')
+ let media = await client.quoted.download();
+ let audio = await toAudio(media,'mp4')
 await client.sendMessage( message.from, { audio: { url:audio }, mimetype: "audio/mpeg", fileName: `${Config.FREE_TXT}.mp3`, }, { quoted: message } );
  });
 bots.inrl({ pattern: ['mp33'], desc: "to convert video to mp3",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
