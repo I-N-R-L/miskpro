46fc01da3a1b3900d3c99532284b7a3eed0ea6d4
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-02 21:52:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
 require("./global");
+const { default: WASocket, DisconnectReason, useSingleFileAuthState, fetchLatestBaileysVersion, jidNormalizedUser, makeInMemoryStore, DEFAULT_CONNECTION_CONFIG, DEFAULT_LEGACY_CONNECTION_CONFIG, } = require("Wa-Web");
 const fs = require("fs");
-const Config = require('./config');
-const { default: WASocket, DisconnectReason, useSingleFileAuthState, fetchLatestBaileysVersion, jidNormalizedUser, makeInMemoryStore, DEFAULT_CONNECTION_CONFIG, DEFAULT_LEGACY_CONNECTION_CONFIG, } = require("@adiwajshing/baileys");
 const chalk = require("chalk");
 const pino = require("pino");
 const path = require("path");
 const { Boom } = require("@hapi/boom");
-const { Simple, upsert, eziofunc } = require("./lib");
+const { Simple, upsert, bixbyfunction } = require("./lib");
 const Welcome = require("./lib/Welcome");
-const jsoConfig = JSON.parse(fs.readFileSync("./lib/database/config.json"));
-const ezio = require("./lib/perfix");
-const { chatting , dbdatas} = eziofunc;
+const jsoConfig = JSON.parse(fs.readFileSync("./lib/config.json"));
+const Config = require("./config");
+const ezio = require("./events");
+const { chatting , circle , faketest} = bixbyfunction;
 const { serialize, WAConnection } = Simple;
 global.mydb = {};
 global.mydb.users = new Array();
 global.mydb.hits = new Number();
 global.isInCmd = false;
 global.catchError = false;
-//varconst fs = require("fs");
 var aes256 = require('aes256');
-let PastebinAPI = require('pastebin-js'),
+let PastebinAPI = require('pastebin-js');
     pastebin = new PastebinAPI({
       'api_dev_key' : 'u_53edsqmFGKd02RMyQPwONVG0bIPi-R',});
-const WhatsBotConnect = async () => {
 const mddc=(Config.SESSION_ID);
 var m = (mddc);
-let mdm = m.replaceAll("inrl~", "");
-var key = 'k!t';
+let mdm = m.replaceAll("bixby~", "");
+var key = 'bixby3';
 var plaintext = (mdm);
 var decryptedPlainText = aes256.decrypt(key, plaintext);
 pastebin
   .getPaste(decryptedPlainText)
-  .then(async function inrlBot(data) {
+  .then(async function bixbyBot(data) {
    fs.writeFileSync("./session.json" , data);
 await console.log('file creted successfully☑️');
-const { state, saveState } = useSingleFileAuthState( "./session.json")
+  });
+const WhatsBotConnect = async () => {
+setTimeout(() => {    
+const { state, saveState } = useSingleFileAuthState( "./session.json", pino({ level: "silent" }) );
 const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }),});
 store.readFromFile("./lib/database/json/baileys/store_multi.json");
 setInterval(() => { store.writeToFile("./lib/database/baileys/store_multi.json")}, 30 * 1000);
 fs.readdirSync("./plugins").forEach((file) => {if (path.extname(file).toLowerCase() == ".js") {require(`./plugins/${file}`);}});
 global.api = (name, path = "/", query = {}, apikeyqueryname) => (name in jsoConfig.APIs ? jsoConfig.APIs[name] : name) + path + (query || apikeyqueryname ? "?" + new URLSearchParams( Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: jsoConfig.APIs.apikey } : {}), }) ) : "");
-if('./session.json'!== true ){
-console.log(' session file cretion failed ');
-};
   let { version, isLatest } = await fetchLatestBaileysVersion();
-  connOptions = { markOnlineOnConnect: true, linkPreviewImageThumbnailWidth: 500, printQRInTerminal: true, browser: ["WhatsBixby", "Safari", "4.0.0"], logger: pino({ level: "silent" }), auth: state, version, };
-  conn = WASocket(connOptions);
+  let connOptions = { markOnlineOnConnect: true, linkPreviewImageThumbnailWidth: 500, printQRInTerminal: true, browser: ["Ai Dark Ezio", "Safari", "4.0.0"], logger: pino({ level: "silent" }), auth: state, version, };
+  let conn = WASocket(connOptions);
   conn = new WAConnection(conn);
   store.bind(conn.ev);
   conn.ev.on("creds.update", saveState);
   conn.ev.on("connection.update", async (update) => {
     const { lastDisconnect, connection, isNewLogin, isOnline, qr, receivedPendingNotifications, } = update;
-    if (connection == "connecting") console.log(chalk.yellow("💖 Connecting to WhatsApp...🥳"));
-    else if (connection == "open") console.log(chalk.green("💖 Login successful! \n bot working now💗")); 
+    if (connection == "connecting") console.log(chalk.yellow("👩 Connecting to WhatsApp...▶"));
+    else if (connection == "open") console.log(chalk.green("👩 Login successful! ▶")); 
     else if (connection == "close") {
       let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
       if (reason === DisconnectReason.badSession) { console.log(chalk.red(`💥 Bad Session File, Please Delete Session and Scan Again`)); conn.logout(); } 
@@ -63,15 +61,15 @@ console.log(' session file cretion failed ');
       else if (reason === DisconnectReason.restartRequired) { console.log(chalk.red("💥 Restart Required, Restarting...")); WhatsBotConnect(); } 
       else if (reason === DisconnectReason.timedOut) { console.log(chalk.red("💥 Connection TimedOut, Reconnecting...")); WhatsBotConnect(); } 
       else conn.end(chalk.red(`💥 Unknown DisconnectReason: ${reason}|${connection}`));
-    } else if (isOnline === true) console.log(chalk.blue("💖 Online."));
-    else if (isOnline === false) console.log(chalk.red("💖 Offine."));
-    else if (receivedPendingNotifications === true) console.log(chalk.blue("💖 Received Pending Notifications."));
-    else if (receivedPendingNotifications === false) console.log(chalk.red("💖 Not Received Pending Notifications."));
-    else if (isNewLogin === true) console.log(chalk.blue("💖 New Login."));
-    else if (isNewLogin === false) console.log(chalk.red("💖 Not New Login."));
+    } else if (isOnline === true) console.log(chalk.blue("👩 Online."));
+    else if (isOnline === false) console.log(chalk.red("👩 Offine."));
+    else if (receivedPendingNotifications === true) console.log(chalk.blue("👩 Received Pending Notifications."));
+    else if (receivedPendingNotifications === false) console.log(chalk.red("👩 Not Received Pending Notifications."));
+    else if (isNewLogin === true) console.log(chalk.blue("👩 New Login."));
+    else if (isNewLogin === false) console.log(chalk.red("👩 Not New Login."));
     else if (qr) console.log(chalk.magenta("Qr: "), chalk.magentaBright(qr));
-    else console.log("💖 Connection...", update);
-   });
+    else console.log("👩 Connection...", update);
+  });
   conn.ev.on("group-participants.update", async (m) => { if (ezio.config.setting.blockchat.includes(m.id)) return; else Welcome(conn, m);});
   conn.ev.on("messages.upsert", async (chatUpdate) => {
     global.isInCmd = false;
@@ -80,43 +78,35 @@ console.log(' session file cretion failed ');
     if (global.mydb.users.indexOf(m.sender) == -1) global.mydb.users.push(m.sender);
     await upsert(conn, m);
     await chatting(m, conn);
-    await dbdatas(m, conn);
     try {
-     ezio.commands.map(async (command) => {
+      ezio.commands.map(async (command) => {
         for (let i in command.pattern) {
-          if (command.pattern[i] == m.client.command || command.on == "text"){
+          if (command.pattern[i] == m.client.command || command.on == "text") {
             global.isInCmd = true; global.mydb.hits += 1; global.catchError = false;
-if(Config.REACT =='true'){
             await conn.sendReact(m.from, await ezio.reactArry("INFO"), m.key);
-}
             await conn.sendPresenceUpdate( ezio.config.auto.presence.value, m.from );
             try {await command.function(m, conn);}
             catch (error) { global.catchError = true; console.log(error); }
-if(Config.REACT =='true'){
-            global.catchError ? await conn.sendReact( m.from, await ezio.reactArry("ERROR"), m.key ) : await conn.sendReact(m.from, command.sucReact, m.key);
-}
-            await conn.sendPresenceUpdate("available", m.from);
+            global.catchError ? await conn.sendReact( m.from, await ezio.reactArry("ERROR"), m.key ) : await conn.sendReact(m.from, command.sucReact, m.key); await conn.sendPresenceUpdate("available", m.from);
           }
         }
       });
-     } catch (e) {
+    } catch (e) {
       console.log(e);
       sendErrorMessage(m.from,e,m.key,m,[],false);
     }
   });
-
-if(Config.U_STATUS =='true'){
   setInterval(async () => {
     var utch = new Date().toLocaleDateString("EN", { weekday: "long", year: "numeric", month: "long", day: "numeric", });
     var ov_time = new Date().toLocaleString("LK", { timeZone: "Asia/Colombo" }).split(" ")[1];
-    const biography = "📅 " + utch + "\n⌚ " + ov_time + "\n\n💗 ᴀᴜᴛᴏ ᴍᴀᴛɪᴄ ʙɪᴏ ʙy ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ";
+    const biography = "📅 " + utch + "\n⌚ " + ov_time + "\n\n💗 Auto Bio Powered By Whats Bot...💬\n\n👨🏼‍💻 Created By Dark_Ezio";
     await conn.updateProfileStatus(biography);
   }, 1000 * 10);
   if (conn.user && conn.user?.id) conn.user.jid = jidNormalizedUser(conn.user?.id); conn.logger = conn.type == "legacy" ? DEFAULT_LEGACY_CONNECTION_CONFIG.logger.child({}) : DEFAULT_CONNECTION_CONFIG.logger.child({});
-             };
-         });
-     };
+    });
+}
 WhatsBotConnect();
+}, 6000);
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
