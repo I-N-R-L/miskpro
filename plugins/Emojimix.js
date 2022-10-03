a7e764f0193c957743cd86291996a3afb39b0a3e
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-10-03 12:47:49 +0530

    Update Emojimix.js

--- plugins/Emojimix.js
+++ plugins/Emojimix.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
 const axios = require('axios');
-const { fetchJson, inrl} = require('../lib');
-inrl({pattern: ['emojimix'], desc: "to get video as audio ", sucReact: "💥", category: ['all'], }, (async (message, client) => {
-		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥺_😹`)
-		for (let res of anu.results) {
-		    await client.sendImageAsSticker(message.from, res.url, message, { packname: "inrl", author: "inrl", categories: res.tags })
-		    await fs.unlinkSync(encmedia)
-		}
-}));
+const {inrl,fetchJson} = require('../lib/');
+const fs = require('fs');
+
+inrl({pattern: ['emojimix'], desc: "to emojis to single sticker",sucReact: "🌇",  category: ["all"]}, async (message, client) => {
+           const text = message.client.text;
+	    if (!text) return await client.sendMessage( message.from, { text: 'send to emojis \n\n _ex_:❣️+🥵'}, { quoted: message });
+if (text.includes('+')) {
+         var split = text.split('+');
+
+         emoji1= split[0];
+         emoji2 = split[1];
+        }
+const url = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
+for (let res of url.results) {
+console.log("inrl="+res.url)
+await client.sendImageAsSticker(message.from, res.url, message, { packname: "inrl", author: "inrl", categories: res.tags })
+        }
+});
 inrl({pattern: ['carbon'], desc: "to get video as audio ", sucReact: "ðŸ’¥", category: ['all'], }, (async (message, client) => {
 
 const text = message.client.text;
