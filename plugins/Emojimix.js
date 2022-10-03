2f5be80704e89cddc331e67a1ffe7181769e103f
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-10-03 12:22:28 +0530

    Update Emojimix.js

--- plugins/Emojimix.js
+++ plugins/Emojimix.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const { fetchJson } = require('../lib/');
-bots.inrl({pattern: ['emojimix'], desc: "to get video as audio ", sucReact: "💥", category: ['all'], }, (async (message, client) => {
+const { fetchJson, inrl} = require('../lib');
+inrl({pattern: ['emojimix'], desc: "to get video as audio ", sucReact: "💥", category: ['all'], }, (async (message, client) => {
 		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=🥺_😹`)
 		for (let res of anu.results) {
 		    await client..sendImageAsSticker(message.from, res.url, message, { packname: "inrl", author: "inrl", categories: res.tags })
