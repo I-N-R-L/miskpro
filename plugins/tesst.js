b1f8c1cebf18ce165da7fd0e8963b3e35ece16b2
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-11 19:05:25 +0530

    Delete tesst.js

--- plugins/tesst.js
+++
@@ -1,4 +0,0 @@
-async (message, client) => {
-	    const url = `https://qr-code-for-bots.herokuapp.com`;
-		    return await client.sendMessage( message.from, { image: { url:  url }, caption: "inrl",},{ quoted: message });
-    };
