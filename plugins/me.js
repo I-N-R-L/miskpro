27d9a3efa6211e67b7dc8e0c583b51df16f71d11
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-12 22:13:22 +0530

    Update me.js

--- plugins/me.js
+++ plugins/me.js
@@ -1,11 +1,10 @@
 const lnn = "ANIME MAKER"
-const { inrl, config, anime, ffpack, ff1,ff2,ff3, ff4, ff5, ff6, ff7, ff8, ff9, ff10, ff11, ff12, ff13, ff14, ff15, ff16, ff17, ff18, ff19, ff20, ff21, ff22, ff23, ff24, ff25, ff26, ff27, ff28, ff29, ff30, ff31, ff32, ff33, ff34, ff35, ff36, ff37, ff38, ff39, ff40, ff41, ff42, ff43, ff44, ff45, ff46, ff47, ff48, ff49, ff50, animepack, an1, an2, an3, an4, an5, an6, an7, an8, an9, an10, an11, an12, an13, an14, an15, an16, an17, bts, robote, spiderman, tentacion, youAreBad, ansay, ch, trumb } = require('../lib')
+const { pass, inrl, config, anime, ffpack, ff1,ff2,ff3, ff4, ff5, ff6, ff7, ff8, ff9, ff10, ff11, ff12, ff13, ff14, ff15, ff16, ff17, ff18, ff19, ff20, ff21, ff22, ff23, ff24, ff25, ff26, ff27, ff28, ff29, ff30, ff31, ff32, ff33, ff34, ff35, ff36, ff37, ff38, ff39, ff40, ff41, ff42, ff43, ff44, ff45, ff46, ff47, ff48, ff49, ff50, animepack, an1, an2, an3, an4, an5, an6, an7, an8, an9, an10, an11, an12, an13, an14, an15, an16, an17, bts, robote, spiderman, tentacion, youAreBad, ansay, ch, trumb } = require('../lib')
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
 const ll ="*```Enter a word```"
 const Ln = "Free Fire logo maker"
-let pass = require('../lib/password');
 let crtPass = pass.PASS;
 let passErr = pass.PERR;
 let checkPass = Config.PASSWORD
@@ -19,13 +18,11 @@ inrl(
     category: ["all","18+"],
   },
   async (message, client) => {
-if(!message.client.text){
-if(checkPass !== crtPass ){
-return await client.sendMessage(message.from,{ text : "😛😐🥴"+passErr }, { quoted: message });
-}
 if(!message.client.isCreator){
     return await client.sendMessage(message.from,{ text :"😼😛🤥"+ passErr }, { quoted: message });
 }
+if(!message.client.text){
+if(checkPass === crtPass ){
 let ttinullimg = youAreBad(); 
 const Message = {
       image: { url: ttinullimg },
@@ -33,6 +30,7 @@ const Message = {
     };
     await client.sendMessage(message.from, Message, { quoted: message });
     global.catchError = false;
+      }
     }
   }
 );
