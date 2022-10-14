378bb87db4252c2c527ac7f04bb8baff33eedb3c
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-14 08:09:07 +0530

    Delete inrlv.js

--- plugins/inrlv.js
+++
@@ -1,9 +0,0 @@
-const { inrl } = require('../lib/perfix');
-const Config = require('../config');
-
-
-    inrl({pattern: ['😕'], desc: "to check whether", sucReact: "💔", category: ['all'],},   async (message, client) => {
-
-	    const url = `https://evilinsult.com/generate_insult.php?lang=en&type=json`;
-		   return await client.sendMessage( message.from, { text:url.insult },{ quoted: message });
-});
