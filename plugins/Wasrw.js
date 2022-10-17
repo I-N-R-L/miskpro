3a0fa6b0b407a24d79e91c85b8668bb35bb9f6b5
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-17 12:39:32 +0530

    Update Wasrw.js

--- plugins/Wasrw.js
+++ plugins/Wasrw.js
@@ -2,24 +2,17 @@
 const { inrl,config }= require('../lib/');
 const fs = require('fs');
 const Config = require('../config');
-const ffmpeg = require('fluent-ffmpeg');
-const White = Config.ALIVE
-const {execFile} = require('child_process');
 
     inrl({ pattern: ['send','snd'], desc: "to create ff logo",sucReact: "⚒️",  category: ["ff"]}, (async (message, client) => {
 
         if (message.quoted.videoMessage) {
         let _message = message.quoted.videoMessage;
-        var location = client.downloadAndSaveMediaMessage(_message)
+        var location = client.downloadMediaMessage(_message)
 await client.sendMessage(message.from, { video: location, caption: config.exif.cap }, { quoted: message });
    await fs.unlinkSync(location)
-      }
-        if (!message.quoted.videoMessage && message.quoted.imageMessage) {
-            ffmpeg(location)
-                .save('status.png')
-                .on('end', async () => {
-                    await client.sendMessage(message.from, { image: status.png, caption: config.exif.cap }, { quoted: message });
-            });
-        return 
-        }
+      }else if (!message.quoted.videoMessage && message.quoted.imageMessage) {
+            let _message = message.quoted.imageMessage;
+            var location = client.downloadMediaMessage(_message)
+                    await client.sendMessage(message.from, { image: location, caption: config.exif.cap }, { quoted: message });
+         }
     }));
