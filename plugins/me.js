b75579fec638ba27753946dc894a42d9bf7e8f61
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-17 12:23:41 +0530

    Update me.js

--- plugins/me.js
+++ plugins/me.js
@@ -1,3 +1,5 @@
+/*
+
 const lnn = "ANIME MAKER"
 const { errorMessage, pass, inrl, config, anime, ffpack, ff1,ff2,ff3, ff4, ff5, ff6, ff7, ff8, ff9, ff10, ff11, ff12, ff13, ff14, ff15, ff16, ff17, ff18, ff19, ff20, ff21, ff22, ff23, ff24, ff25, ff26, ff27, ff28, ff29, ff30, ff31, ff32, ff33, ff34, ff35, ff36, ff37, ff38, ff39, ff40, ff41, ff42, ff43, ff44, ff45, ff46, ff47, ff48, ff49, ff50, animepack, an1, an2, an3, an4, an5, an6, an7, an8, an9, an10, an11, an12, an13, an14, an15, an16, an17, bts, robote, spiderman, tentacion, youAreBad, ansay, ch, trumb, inrlQuita, insult }  = require('../lib')
 const fs = require('fs');
@@ -1408,3 +1410,4 @@ inrl({pattern: ['🥲',"😦","🥺","😑","😕","🤧","😔","🙄","🤥","
    const texts = await insult();
    await client.sendMessage( message.from, { text : "😹"+texts },{ quoted: message });
 });
+*/
