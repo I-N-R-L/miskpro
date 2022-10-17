b3903d1e672fecd58f57186307fef59e691efa29
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-17 12:43:30 +0530

    Update Wasrw.js

--- plugins/Wasrw.js
+++ plugins/Wasrw.js
@@ -6,13 +6,11 @@ const Config = require('../config');
     inrl({ pattern: ['send','snd'], desc: "to create ff logo",sucReact: "⚒️",  category: ["ff"]}, (async (message, client) => {
 
         if (message.quoted.videoMessage) {
-        let _message = message.quoted.videoMessage;
-        var location = client.downloadMediaMessage(_message)
+       let location = await message.quoted.download();
 await client.sendMessage(message.from, { video: location, caption: config.exif.cap }, { quoted: message });
    await fs.unlinkSync(location)
       }else if (!message.quoted.videoMessage && message.quoted.imageMessage) {
-            let _message = message.quoted.imageMessage;
-            var location = client.downloadMediaMessage(_message)
+            let location =await message.quoted.download()
                     await client.sendMessage(message.from, { image: location, caption: config.exif.cap }, { quoted: message });
          }
     }));
