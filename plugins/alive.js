0dd5573f12c2a0a5604d785a0fe22c3646a7bcc0
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-10-19 06:12:25 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -114,7 +114,7 @@ if (aliveImgUrl.endsWith('.mp4')) {
       video: { url: aliveImgUrl },
       caption: `${alievTxtNew}`,
       footer: config.exif.footer,
-      buttons,
+      await buttons,
     }
 await client.sendMessage(message.from, templateButtons, { quoted: message });
 }else if(aliveImgUrl.endsWith('.jpg')) {
@@ -122,7 +122,7 @@ await client.sendMessage(message.from, templateButtons, { quoted: message });
       image: { url: aliveImgUrl },
       caption: `${alievTxtNew}`,
       footer: config.exif.footer,
-      buttons,
+      await buttons,
     }
 await client.sendMessage(message.from, templateButtons, { quoted: message });
 }else if(aliveImgUrl.endsWith('.jpeg')) {
@@ -130,7 +130,7 @@ await client.sendMessage(message.from, templateButtons, { quoted: message });
       image: { url: aliveImgUrl },
       caption: `${alievTxtNew}`,
       footer: config.exif.footer,
-      buttons,
+      await buttons,
     }
 await client.sendMessage(message.from, templateButtons, { quoted: message });
 } else {
