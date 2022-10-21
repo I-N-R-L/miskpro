60599263c996d4150ab9b0c1d0e8e15229399c2f
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-21 22:36:23 +0530

    Update IsJid.js

--- plugins/IsJid.js
+++ plugins/IsJid.js
@@ -22,13 +22,9 @@ inrl(
 try{
 		let { participants } = await conn.groupMetadata(m.id)
 		let gParticipants = m.participants
-		let teks = `╚»˙·٠•●♥ Tag All ♥●•٠·˙«╝ 
- 
-                ➲ *Message : ${mesage.client.text}*\n\n`
+		for (let mem of gParticipants) {
 
-                for (let mem of gParticipants) {
-
-                teks += ` @${mem.split('@')[0]}\n`
+                let teks += await `@${mem.split('@')[0]}\n`
 
                
                 await client.sendMessage(message.from, { text: teks })
