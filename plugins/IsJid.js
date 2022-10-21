47cc9a34da6e8f26ed39eee49a4eda85b3c48f0e
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-21 21:55:28 +0530

    Update IsJid.js

--- plugins/IsJid.js
+++ plugins/IsJid.js
@@ -10,3 +10,18 @@ inrl(
 		return await client.sendMessage( message.from, { text: message.from }, { quoted: message })
                 }
 )
+inrl(
+	   {
+		pattern: ['tagall'],
+		desc: 'To check ping',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+	async (message, client) => {
+
+		let { id, owner, subject, subjectOwner, subjectTime, creation, desc, descOwner, descId, restrict, announce, size, participants, ephemeralDuration, } = await client.groupMetadata(message.id)
+		let gParticipants = message.participants
+		let texts += await participants;
+		return await client.sendMessage( message.from, { text: texts }, { quoted: message })
+                }
+)
