c4ec6c86d318462ff9f1491a2e4751d384512221
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-21 21:38:16 +0530

    Create IsJid.js

---
+++ plugins/IsJid.js
@@ -0,0 +1,11 @@
+inrl(
+	   {
+		pattern: ['jid'],
+		desc: 'To check ping',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+	async (message, client) {
+		return await client.sendMessage( message.from, { text: message.from }, { quoted: message })
+                }
+)
