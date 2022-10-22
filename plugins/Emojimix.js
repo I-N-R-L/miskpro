0e6963f43f63ddd15d0e544ad1595c51797905a0
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-22 14:36:07 +0530

    Update Emojimix.js

--- plugins/Emojimix.js
+++ plugins/Emojimix.js
@@ -13,7 +13,7 @@ if (text.includes('+')) {
 const url = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 for (let res of url.results) {
 console.log("inrl="+res.url)
-await client.sendImageAsSticker(message.from, res.url, message, { packname: "inrl", author: "inrl", categories: res.tags })
+await client.sendImageAsSticker(message.from,{ url :res.url }, message, { packname: "inrl", author: "inrl", categories: "🥵" })
         }
 });
 inrl({pattern: ['carbon'], desc: "to get video as audio ", sucReact: "ðŸ’¥", category: ['all'], }, (async (message, client) => {
