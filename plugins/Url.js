fc222dbfce261a8d8160c16721f14367fbe5b28a
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-22 07:02:14 +0530

    Update Url.js

--- plugins/Url.js
+++ plugins/Url.js
@@ -43,3 +43,19 @@ const Message = { image: { url:  ttinullimage }, caption: bots.config.exif.cap, 
 
         await client.sendMessage( message.from, Message,{ quoted: message })
 });
+const Raashii = require('../lib/perfix');
+const Config = require('../config');
+const { getBuffer } = require('../lib/cloud');
+const need = "*add link after commandðŸ’Œ*"
+
+  Raashii.inrl({ pattern: ['pdf'], desc: "to get web screenshot",sucReact: "⚒️",  category: ["all"], iscmd : "pdf", type:"misc"}, (async (message, client) => {
+
+const text = message.client.text;
+
+    if (!text) return await client.sendMessage(message.from, { text :need },{ quoted: message })
+
+    var inrlmx = await getBuffer(`https://api.html2pdf.app/v1/generate?url=${text}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`)
+    
+    await client.sendMessage(message.from , {document: inrlmx, mimetype: 'application/pdf', fileName: `inrl.pdf`}, {quoted: message})
+
+  }))
