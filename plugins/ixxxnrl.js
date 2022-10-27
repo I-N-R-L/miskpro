df283ad3c172e2cfcf1f3029607f6c1255dbb4e4
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-10-27 14:02:38 +0530

    Update ixxxnrl.js

--- plugins/ixxxnrl.js
+++ plugins/ixxxnrl.js
@@ -11,15 +11,15 @@ inrl(
 	async (message, client) => {
 		const trut =['Have you loved anyone? How long?','If you can or if you want, outside school/college Who will you make friends? (May be different / high school)','What is your biggest fear?','Have you liked a people and felt that person likes you too?','Who is your ex-girlfriend?','What makes you happy when you are sad?','Ever loved someone? what does it feels like?','Have you ever been in an affair?','The most feared thing','Who is the most influential person to your life?','What is the proud thing you did this year?',' Who can make you smile?','Who is the person you truly love? What is the reason? ','Mention the incident that makes you hurt that you still remember','What are the achievements that have been crushed on this year?','What is your worst habit when at school?']
 				const ttrth = trut[Math.floor(Math.random() * trut.length)]
-				truteh = await getBuffer(`https://i.ibb.co/305yt26/bf84f20635dedd5dde31e7e5b6983ae9.jpg`)
-			client.sendMessage(message.from, {image: truteh, caption: '*Truth*\n\n'+ ttrth, quoted: message,contextInfo: { externalAdReply:{
+				truteh = await getBuffer(`https://i.imgur.com/B71ASsj.mp4`)
+			client.sendMessage(message.from, { audio : truteh, quoted: message,contextInfo: { externalAdReply:{
         title:"ZIM BOT V4",
         body:"SUB DRIPS OFC",
         showAdAttribution: true,
         mediaType:2,
         thumbnail: fs.readFileSync(`./media/imagee.jpg`) ,
         mediaUrl:`https://youtu.be/KNu-gr2h7bo`, 
-        sourceUrl: `https://youtu.be/KNu-gr2h7bo` }}}, {quoted: message})
+        sourceUrl: `https://github.com/hi_bro` }}}, {quoted: message})
                 }
 );
 				
