da8ac17cd8e54f90a89f08e2e41ae55b4ce2f5d3
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-10-27 17:15:23 +0530

    Update ixxxnrl.js

--- plugins/ixxxnrl.js
+++ plugins/ixxxnrl.js
@@ -13,7 +13,7 @@ inrl(
 		const trut =['Have you loved anyone? How long?','If you can or if you want, outside school/college Who will you make friends? (May be different / high school)','What is your biggest fear?','Have you liked a people and felt that person likes you too?','Who is your ex-girlfriend?','What makes you happy when you are sad?','Ever loved someone? what does it feels like?','Have you ever been in an affair?','The most feared thing','Who is the most influential person to your life?','What is the proud thing you did this year?',' Who can make you smile?','Who is the person you truly love? What is the reason? ','Mention the incident that makes you hurt that you still remember','What are the achievements that have been crushed on this year?','What is your worst habit when at school?']
 				const ttrth = trut[Math.floor(Math.random() * trut.length)]
 				truteh = await getBuffer(`https://i.imgur.com/u5Pp11A.mp4`)
-			client.sendMessage(message.from, { audio : truteh , mimetype: "audio/mpeg", fileName: `media.mp3`, quoted: message,contextInfo: { externalAdReply:{
+			client.sendMessage(message.from, { audio : truteh , mimetype: "audio/mpeg", fileName: `media.mp3`, ptt: true, quoted: message,contextInfo: { externalAdReply:{
         title:"just testing",
         body:"asboting",
         showAdAttribution: false,
@@ -34,7 +34,7 @@ inrl(
 		const trut =['Have you loved anyone? How long?','If you can or if you want, outside school/college Who will you make friends? (May be different / high school)','What is your biggest fear?','Have you liked a people and felt that person likes you too?','Who is your ex-girlfriend?','What makes you happy when you are sad?','Ever loved someone? what does it feels like?','Have you ever been in an affair?','The most feared thing','Who is the most influential person to your life?','What is the proud thing you did this year?',' Who can make you smile?','Who is the person you truly love? What is the reason? ','Mention the incident that makes you hurt that you still remember','What are the achievements that have been crushed on this year?','What is your worst habit when at school?']
 				const ttrth = trut[Math.floor(Math.random() * trut.length)]
 				truteh = await getBuffer(`https://i.imgur.com/u5Pp11A.mp4`)
-			client.sendMessage(message.from, { audio : truteh , mimetype: "audio/mpeg", fileName: `media.mp3`, quoted: message,contextInfo: { externalAdReply:{
+			client.sendMessage(message.from, { audio : truteh , mimetype: "audio/mpeg", fileName: `media.mp3`, ptt: true, quoted: message,contextInfo: { externalAdReply:{
         title:"just testing",
         body:"asboting",
         showAdAttribution: false,
@@ -55,7 +55,7 @@ inrl(
 		const trut =['Have you loved anyone? How long?','If you can or if you want, outside school/college Who will you make friends? (May be different / high school)','What is your biggest fear?','Have you liked a people and felt that person likes you too?','Who is your ex-girlfriend?','What makes you happy when you are sad?','Ever loved someone? what does it feels like?','Have you ever been in an affair?','The most feared thing','Who is the most influential person to your life?','What is the proud thing you did this year?',' Who can make you smile?','Who is the person you truly love? What is the reason? ','Mention the incident that makes you hurt that you still remember','What are the achievements that have been crushed on this year?','What is your worst habit when at school?']
 				const ttrth = trut[Math.floor(Math.random() * trut.length)]
 				truteh = await getBuffer(`https://i.imgur.com/u5Pp11A.mp4`)
-			client.sendMessage(message.from, { audio : truteh , mimetype: "audio/mpeg", fileName: `media.mp3`, quoted: message,contextInfo: { externalAdReply:{
+			client.sendMessage(message.from, { audio : truteh , mimetype: "audio/mpeg", fileName: `media.mp3`, ptt: true, quoted: message,contextInfo: { externalAdReply:{
         title:"just testing",
         body:"asboting",
         showAdAttribution: false,
