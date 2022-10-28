cbfbed48b5dfda4530374ccf53f13e630c43841d
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-10-28 07:40:46 +0530

    Update ixxxnrl.js

--- plugins/ixxxnrl.js
+++ plugins/ixxxnrl.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { inrl, getBuffer } = require('../lib/');
-cosnt Config = require('../config');
+const Config = require('../config');
 //const { getLastMessageInChat } = require('@adiwajshing/baileys');
 
 inrl(
@@ -12,7 +12,7 @@ inrl(
 	   },
 	async (message, client) => {
 console.log(client.user.id)
-		const trut =['Have you loved anyone? How long?','If you can or if you want, outside school/college Who will you make friends? (May be different / high school)','What is your biggest fear?','Have you liked a people and felt that person likes you too?','Who is your ex-girlfriend?','What makes you happy when you are sad?','Ever loved someone? what does it feels like?','Have you ever been in an affair?','The most feared thing','Who is the most influential person to your life?','What is the proud thing you did this year?',' Who can make you smile?','Who is the person you truly love? What is the reason? ','Mention the incident that makes you hurt that you still remember','What are the achievements that have been crushed on this year?','What is your worst habit when at school?']
+	/*	const trut =['Have you loved anyone? How long?','If you can or if you want, outside school/college Who will you make friends? (May be different / high school)','What is your biggest fear?','Have you liked a people and felt that person likes you too?','Who is your ex-girlfriend?','What makes you happy when you are sad?','Ever loved someone? what does it feels like?','Have you ever been in an affair?','The most feared thing','Who is the most influential person to your life?','What is the proud thing you did this year?',' Who can make you smile?','Who is the person you truly love? What is the reason? ','Mention the incident that makes you hurt that you still remember','What are the achievements that have been crushed on this year?','What is your worst habit when at school?']
 				const ttrth = trut[Math.floor(Math.random() * trut.length)]
 				truteh = await getBuffer(Config.)
 			client.sendMessage(message.from, { audio : truteh , mimetype: "audio/mpeg", fileName: `media.mp3`, ptt: true, quoted: message,contextInfo: { externalAdReply:{
@@ -22,6 +22,6 @@ console.log(client.user.id)
         mediaType:2,
         thumbnail: fs.readFileSync(`./media/imagee.jpg`) ,
         mediaUrl:`https://github.com/hi_bro`, 
-        sourceUrl: `https://github.com/hi_bro` }}}, {quoted: message})
+        sourceUrl: `https://github.com/hi_bro` }}}, {quoted: message})*/
                 }
 );
