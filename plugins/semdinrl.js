7ca91e417abbd8ee22d8aee1c55dfa5e5f5e834c
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 09:45:46 +0530

    Update semdinrl.js

--- plugins/semdinrl.js
+++ plugins/semdinrl.js
@@ -1,6 +1,15 @@
-const { inrl } = require('../lib');
-const virtext = require('../media/virtex');
+const { inrl, getBuffer } = require('../lib');
+const Config = require('../config');
 
 inrl({ pattern: ['virtext'], desc: "to get web screenshot",sucReact: "⚒️",  category: ["all"],}, (async (message, client) => {
-     await client.sendMessage(message.from, {text : "inrl"+virtext.inrlvirtext });
+     await client.sendMessage(message.from, {
+text : "inrl",
+contextInfo:{externalAdReply:{
+title: "${ownername}",
+body: `Don't forget to read group description`,
+mediaType:2,
+thumbnail: getBuffer(Config.BOT_INFO.split(',')[2],
+sourceUrl: "https://g.com",
+mediaUrl: `https://g.com`
+}} });
     }))
