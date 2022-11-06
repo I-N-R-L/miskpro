b8d8edd3f376d2725b63657bdbb4ad77db235172
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 18:12:09 +0530

    Update sticker_pack.js

--- plugins/sticker_pack.js
+++ plugins/sticker_pack.js
@@ -4,5 +4,9 @@ const Config = require('../config');
 
 inrl({ pattern: ['dogsticker'], desc: "to get random dog stickers",sucReact: "🐕",  category: ["sticker"]}, async (message, client) => {
 let sticker = await dogsticker();
-await client.sendImageAsSticker(message.from, sticker, message, { packname: Config.STICKER_DATA.split(',')[0], author: Config.STICKER_DATA.split(',')[1], })
+await client.sendMessage( message.from, { sticker: sticker }, { quoted: message } );
+});
+inrl({ pattern: ['dogsticke'], desc: "to get random dog stickers",sucReact: "🐕",  category: ["sticker"]}, async (message, client) => {
+let sticker = await dogsticker();
+await client.sendMessage( message.from, { sticker: Buffer.from(sticker) }, { quoted: message } );
 });
