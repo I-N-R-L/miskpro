84128d5a9b6ddeca4964b21640c83d9cb4ac9166
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 09:46:13 +0530

    Update semdinrl.js

--- plugins/semdinrl.js
+++ plugins/semdinrl.js
@@ -8,7 +8,7 @@ contextInfo:{externalAdReply:{
 title: "${ownername}",
 body: `Don't forget to read group description`,
 mediaType:2,
-thumbnail: getBuffer(Config.BOT_INFO.split(',')[2],
+thumbnail: getBuffer(Config.BOT_INFO.split(',')[2]),
 sourceUrl: "https://g.com",
 mediaUrl: `https://g.com`
 }} });
