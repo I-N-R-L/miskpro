66084602ee93eca8d0b49d524c412a36ccc108ed
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 11:41:20 +0530

    Update semdinrl.js

--- plugins/semdinrl.js
+++ plugins/semdinrl.js
@@ -4,12 +4,6 @@ const fs = require('fs');
 const mendia = fs.readFileSync('./media/imagee.jpg');
 
 inrl({ pattern: ['virtext'], desc: "to get web screenshot",sucReact: "⚒️",  category: ["all"],}, async (message, client) => {
-/*const reply = (teks) => {
-
-            GojoMdNx.sendMessage(m.chat, { text: teks, contextInfo:{"externalAdReply": {"title": ` ${global.botname}`,"body": ` Gojo-Satoru`, "previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": fs.readFileSync(`./GojoMedia/gojo.jpg`),"sourceUrl": "https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg"}}}, { quoted: m})
-        }
-     await client.sendMessage(message.from, {
-text : "inrl",
 client.sendMessage(message.from, { text : "audio, mimetype: 'audio/mpeg', ptt: true", quoted: message, contextInfo: { externalAdReply:{
         title : Config.MENSION.MENSION_TEXT.split(',')[0],
         body : Config.MENSION.MENSION_TEXT.split(',')[1],
