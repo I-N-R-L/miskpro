fe21dfa9ac77387918e3de9a480c21a17b7ef869
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 11:40:55 +0530

    Update semdinrl.js

--- plugins/semdinrl.js
+++ plugins/semdinrl.js
@@ -10,17 +10,13 @@ inrl({ pattern: ['virtext'], desc: "to get web screenshot",sucReact: "⚒️",  
         }
      await client.sendMessage(message.from, {
 text : "inrl",
-jpegThumbnail : mendia,
-headerType : 4,
-contextInfo : {externalAdReply:{
-title : "${ownername}",
-body : `Don't forget to read group description`,
-mediaType : 2,
-thumbnail : getBuffer(Config.BOT_INFO.split(',')[2]),
-sourceUrl : "https://g.com",
-mediaUrl : `https://g.com`
-     }}})
-});
-*/
-client.sendMessage(message.from, { text: "error", contextInfo:{"externalAdReply": {"title": ` ${Config.OWNER}`,"body": ` Gojo-Satoru`, "previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": fs.readFileSync(`./media/imagee.jpg`),"sourceUrl": "https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg"}}}, { quoted: message})
+client.sendMessage(message.from, { text : "audio, mimetype: 'audio/mpeg', ptt: true", quoted: message, contextInfo: { externalAdReply:{
+        title : Config.MENSION.MENSION_TEXT.split(',')[0],
+        body : Config.MENSION.MENSION_TEXT.split(',')[1],
+        showAdAttribution: true,
+        mediaType:1,
+        thumbnail: fs.readFileSync('./media/imagee.jpg'),
+        mediaUrl: "https://instagram.com/hi_bro"/*Config.MENSION.MENSION_TEXT.split(',')[2]*/, 
+        sourceUrl: "https://instagram.com/hi_bro"/*Config.MENSION.MENSION_TEXT.split(',')[2]*/ }}}, {quoted: message })
+//client.sendMessage(message.from, { text: "error", contextInfo:{"externalAdReply": {"title": ` ${Config.OWNER}`,"body": ` Gojo-Satoru`, "previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": fs.readFileSync(`./media/imagee.jpg`),"sourceUrl": "https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg"}}}, { quoted: message})
    })
