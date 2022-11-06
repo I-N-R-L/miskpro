17391bab224bb1f41d8d4254e9ecc6462f287495
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 10:02:06 +0530

    Update semdinrl.js

--- plugins/semdinrl.js
+++ plugins/semdinrl.js
@@ -1,15 +1,19 @@
 const { inrl, getBuffer } = require('../lib');
 const Config = require('../config');
+const fs = require('fs');
+const mendia = fs.readFileSync('./media/imagee.jpg');
 
 inrl({ pattern: ['virtext'], desc: "to get web screenshot",sucReact: "⚒️",  category: ["all"],}, async (message, client) => {
      await client.sendMessage(message.from, {
 text : "inrl",
-contextInfo:{externalAdReply:{
-title: "${ownername}",
-body: `Don't forget to read group description`,
-mediaType:2,
-thumbnail: getBuffer(Config.BOT_INFO.split(',')[2]),
-sourceUrl: "https://g.com",
-mediaUrl: `https://g.com`
+jpegThumbnail : mendia,
+headerType : 4,
+contextInfo : {externalAdReply:{
+title : "${ownername}",
+body : `Don't forget to read group description`,
+mediaType : 2,
+thumbnail : getBuffer(Config.BOT_INFO.split(',')[2]),
+sourceUrl : "https://g.com",
+mediaUrl : `https://g.com`
      }}})
 });
