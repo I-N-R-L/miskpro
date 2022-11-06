fe10cbc57e805c050dbefd1021168dade8fc0b1d
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 15:00:40 +0530

    Update semdinrl.js

--- plugins/semdinrl.js
+++ plugins/semdinrl.js
@@ -1,11 +1,23 @@
 const { inrl, getBuffer } = require('../lib');
-const { animewifu } = require('../lib/database/anime_api_pack');
-const Config = require('../config');
-const fs = require('fs');
-const mendia = fs.readFileSync('./media/imagee.jpg');
+const { animewifu, animenom } = require('../lib/database/anime_api_pack');
 
 inrl({ pattern: ['animewifu'], desc: "to get web screenshot",sucReact: "⚒️",  category: ["all"],}, async (message, client) => {
 let ttimg = await animewifu();
+let buttons = [
+        {buttonId:'.animewifu', buttonText: {displayText: `ɴᴇxᴛ ➪`}, type: 1},
+      ]
+      let buttonMsg = {
+      image: {url:ttimg.data.url},
+      caption:  `Here you go!`,
+      footer: Config.FOOTER,
+      buttons: buttons,
+      headerType: 4
+      }
+await client.sendMessage(message.from, buttonMsg, {quoted: message})
+})
+
+inrl({ pattern: ['animewifu'], desc: "to get web screenshot",sucReact: "⚒️",  category: ["all"],}, async (message, client) => {
+let ttimg = await animenom();
 let buttons = [
         {buttonId:'.animewifu', buttonText: {displayText: `ɴᴇxᴛ ➪`}, type: 1},
       ]
