e200b3ec51bb84530780a5c3641950fba720432d
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 17:49:51 +0530

    Update sticker_pack.js

--- plugins/sticker_pack.js
+++ plugins/sticker_pack.js
@@ -1,1 +1,8 @@
 const { inrl, dogsticker, lovesticker, cartoonsticker } = require('../lib');
+const fs = require('fs');
+const Config = require('../config');
+
+inrl({ pattern: ['dogsticker'], desc: "to get random dog stickers",sucReact: "🐕",  category: ["sticker"]}, async (message, client) => {
+let sticker = await dogsticker();
+await client.sendImageAsSticker(message.from, sticker, message, { packname: Config.STICKER_DATA.split(',')[0], author: Config.STICKER_DATA.split(',')[1], })
+});
