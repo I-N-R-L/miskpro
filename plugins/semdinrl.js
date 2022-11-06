f5ef1ba0ee15537eb16abe748d4a9de8aed3ed7a
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-06 10:14:30 +0530

    Update semdinrl.js

--- plugins/semdinrl.js
+++ plugins/semdinrl.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const mendia = fs.readFileSync('./media/imagee.jpg');
 
 inrl({ pattern: ['virtext'], desc: "to get web screenshot",sucReact: "⚒️",  category: ["all"],}, async (message, client) => {
+/*const reply = (teks) => {
+
+            GojoMdNx.sendMessage(m.chat, { text: teks, contextInfo:{"externalAdReply": {"title": ` ${global.botname}`,"body": ` Gojo-Satoru`, "previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": fs.readFileSync(`./GojoMedia/gojo.jpg`),"sourceUrl": "https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg"}}}, { quoted: m})
+        }
      await client.sendMessage(message.from, {
 text : "inrl",
 jpegThumbnail : mendia,
@@ -17,3 +21,6 @@ sourceUrl : "https://g.com",
 mediaUrl : `https://g.com`
      }}})
 });
+*/
+client.sendMessage(message.from, { text: "error", contextInfo:{"externalAdReply": {"title": ` ${Config.OWNER}`,"body": ` Gojo-Satoru`, "previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": fs.readFileSync(`./media/imagee.jpg`),"sourceUrl": "https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg"}}}, { quoted: message})
+   })
