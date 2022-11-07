5a83f9a05074a8b8a60d7a65843f0378afd05a9d
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-07 17:15:10 +0530

    Update sampledownloader.js

--- plugins/sampledownloader.js
+++ plugins/sampledownloader.js
@@ -64,7 +64,7 @@ inrl(
 	   },
 	async (message, client) => {
         if (!isUrl(message.client.args[0]) && !message.client.args[0].includes('mediafire.com')) return await client.sendMessage( message.from, { text :`The link you provided is invalid` })
-        if (firstData[0].size.split('MB')[0] >= 999) return await client.sendMessage( message.from, { text :'*File Over Limit* '+util.format(firstData)})
+        if (mediaFire(message.client.text).firstData[0].size.split('MB')[0] >= 999) return await client.sendMessage( message.from, { text :'*File Over Limit* '+util.format(firstData)})
         if(message.client.text){
         await client.sendMessage( message.from, { text: await mediaFire(message.client.text).result })
         //return await client.sendMessage( message.from, { document : { url : await mediaFire(message.client.text).firstData[0].link}, fileName : await mediaFire(message.client.text).firstData[0].nama, mimetype: await mediaFire(message.client.text).firstData[0].mime }, { quoted: message })
