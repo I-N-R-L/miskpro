db3c4b2d86d6f8a5db1d17e3500120c6e89677e5
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-07 16:26:34 +0530

    Update sampledownloader.js

--- plugins/sampledownloader.js
+++ plugins/sampledownloader.js
@@ -2,7 +2,7 @@ const { inrl, googleIt, wikiMedia, ringTone, mediaFire, gitClone, happyMod } = r
 inrl(
 	   {
 		pattern: ['google'],
-		desc: 'do get goole aerch rsult',
+		desc: 'do get goole serch result',
                 sucReact: "🙃",
                 category: ["system", "all"],
 	   },
@@ -10,6 +10,20 @@ inrl(
         if(message.client.text){
         let teks = await googleIt(message.client.text);
         return await client.sendMessage( message.from, { text: "result =>\n\n"+teks }, { quoted: message })
+          }
+     }
+);
+inrl(
+	   {
+		pattern: ['wikimedia'],
+		desc: 'do get data from wikimedia',
+                sucReact: "🙃",
+                category: ["system", "all"],
+	   },
+	async (message, client) => {
+        if(message.client.text){
+        let teks = await wikiMedia(message.client.text);
+        return await client.sendMessage( message.from, { text: "_result_\n\n"+teks }, { quoted: message })
           }
      }
 );
