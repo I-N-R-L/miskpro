73f921b247fd17cfc97c8385169b828f0ada8888
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-11 20:34:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ if(Config.PM_BLOCK == "true"){
     try {
      inrl.commands.map(async (command) => {
         for (let i in command.pattern) {
-console.log(command.on)
+console.log(command.noncmd)
           if (command.pattern[i] == m.client.command){
             global.isInCmd = true; global.mydb.hits += 1; global.catchError = false;
             if(Config.REACT =='true'){
