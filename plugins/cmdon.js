62d042ad53ffbbe6c9214c6b240fbf53a50fd132
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-11 19:59:58 +0530

    Create cmdon.js

---
+++ plugins/cmdon.js
@@ -0,0 +1,14 @@
+const { inrl } = require('../lib');
+
+inrl(
+	   {
+		on : "text" ,
+		desc: 'To check ping',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+	async (message, client) => {
+	console.log("inrlonmsg working perfectly')
+//return await client.sendMessage( message.from, { text: message.from }, { quoted: message })
+                }
+);
