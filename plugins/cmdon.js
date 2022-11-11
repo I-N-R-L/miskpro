95a03f989ea018531a1f12ad21bddf90ba4dc55e
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-11 20:04:54 +0530

    Update cmdon.js

--- plugins/cmdon.js
+++ plugins/cmdon.js
@@ -8,7 +8,7 @@ inrl(
                 category: ["system", "all"],
 	   },
 	async (message, client) => {
-	console.log("inrlonmsg working perfectly")
-//return await client.sendMessage( message.from, { text: message.from }, { quoted: message })
+	//console.log("inrlonmsg working perfectly")
+return await client.sendMessage( message.from, { text: message.from }, { quoted: message })
                 }
 );
