f216dc7885fe2ad586aec28406365bc2d5e04bf9
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-12 21:30:43 +0530

    Update cmdon.js

--- plugins/cmdon.js
+++ plugins/cmdon.js
@@ -20,7 +20,7 @@ inrl(
 	   },
 	async (message, client, text, cmd, store) => {
 let anu = await hentaivideo()
-                result912 = anu[Math.floor(Math.random(), anu.length)]
+               let result912 = anu[Math.floor(Math.random(), anu.length)]
 cosnole.log(result912.video_1);
               return await client.sendMessage(message.chat, { video: { url: result912.video_1 }, caption: ` Title : result912.titlvideo_1` }, { quoted: message })
        }
