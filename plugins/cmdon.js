17a754a06a2d8c7593a8401d6d7f5990fa9f8bdc
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-12 22:17:38 +0530

    Update cmdon.js

--- plugins/cmdon.js
+++ plugins/cmdon.js
@@ -14,20 +14,6 @@ await send_vote(message, client);
 );
 inrl(
 	   {
-		pattern : ["hentaivideo"] ,
-		desc: 'To check ping',
-                sucReact: "💯",
-                category: ["system", "all"],
-	   },
-	async (message, client, text, cmd, store) => {
-let anu = await hentaivideo()
-               let result912 = anu[Math.floor(Math.random(), anu.length)]
-console.log(result912.video_1);
-              return await client.sendMessage(message.from, { video: { url: result912.video_1 }, caption: ` Title : result912.titlvideo_1` }, { quoted: quoted(message).status })
-       }
-);
-inrl(
-	   {
 		pattern : ["textt"] ,
 		desc: 'To check ping',
                 sucReact: "💯",
