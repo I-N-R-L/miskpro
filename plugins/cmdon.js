7b23616210a7254001d07baf6b492c3ce89052ad
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-12 21:39:59 +0530

    Update cmdon.js

--- plugins/cmdon.js
+++ plugins/cmdon.js
@@ -25,3 +25,44 @@ console.log(result912.video_1);
               return await client.sendMessage(message.from, { video: { url: result912.video_1 }, caption: ` Title : result912.titlvideo_1` }, { quoted: message })
        }
 );
+inrl(
+	   {
+		pattern : ["text"] ,
+		desc: 'To check ping',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+	async (message, client, text, cmd, store) => {
+let pollCreation = generateWAMessageFromContent(message.chat, proto.Message.fromObject({
+"pollCreationMessage": {
+"name": "botname",
+"options": [
+	{
+"optionName": "VOTE FOR PLEASURE"
+	},
+	{
+"optionName": "VOTE AND WIN IPHONE 14 PRO MAX"
+	},
+	{
+"optionName": "VOTE TO GET FREE +84 NUMBER"
+	},
+	{
+"optionName": "VOTE TO GET +54"
+	},
+	{
+"optionName": "VOTE TO GET +64"
+	}
+],
+"selectableOptionsCount": 5
+	}
+}), { userJid: message.from, quoted: message })
+
+await client.relayMessage(message.from, pollCreation.message, { messageId: pollCreation.key.id })
+
+//await client.sendMessage(i.id, { text:" message.from" })
+       // return await sleep(1000)
+           //   }
+       }
+);
+       }
+);
