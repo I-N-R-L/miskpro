fea4d71e2c8731e001e05fc047f03c9ea2e39943
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-12 20:45:38 +0530

    Update cmdon.js

--- plugins/cmdon.js
+++ plugins/cmdon.js
@@ -9,11 +9,5 @@ inrl(
 	   },
 	async (message, client, text, cmd, store) => {
 
-let id = message.client.args && /\d+\-\d+@g.us/.test(message.client.args[0]) ? message.client.args[0] : message.chat
-                    let online = [...Object.keys(store.presences[id]), message.client.botNumber]
-          //  XeonBotInc.sendText(m.chat, 'Online List:\n\n' + online.map(v => ' @' + v.replace(/@.+/, '')).join`\n`, m, { mentions: online })
-await client.sendMessage(message.from, { text:'Online List:\n\n' + online.map(v => ' @' + v.replace(/@.+/, '')).join`\n`, message }, { mentions: online })
-       // return await sleep(1000)
-           //   }
        }
 );
