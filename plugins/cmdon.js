010f86bb5339e76305f16e0c31eb440284a0593b
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-12 22:03:15 +0530

    Update cmdon.js

--- plugins/cmdon.js
+++ plugins/cmdon.js
@@ -1,5 +1,4 @@
-const { inrl, quoted, hentaivideo, send_vote, sleep } = require('../lib');
-const { generateWAMessageFromContent, proto } = require('@adiwajshing/baileys')
+const { inrl, quoted, hentaivideo, send_vote, send_poll } = require('../lib');
 inrl(
 	   {
 		pattern : ["text"] ,
@@ -32,35 +31,7 @@ inrl(
                 sucReact: "💯",
                 category: ["system", "all"],
 	   },
-	async (message, client, text, cmd, store) => {
-let pollCreation = generateWAMessageFromContent(message.from, proto.Message.fromObject({
-"pollCreationMessage": {
-"name": "botname",
-"options": [
-	{
-"optionName": "VOTE FOR PLEASURE"
-	},
-	{
-"optionName": "VOTE AND WIN IPHONE 14 PRO MAX"
-	},
-	{
-"optionName": "VOTE TO GET FREE +84 NUMBER"
-	},
-	{
-"optionName": "VOTE TO GET +54"
-	},
-	{
-"optionName": "VOTE TO GET +64"
-	}
-],
-"selectableOptionsCount": 5
-	}
-}), { userJid: message.from, quoted: message })
-
-await client.relayMessage(message.from, pollCreation.message, { messageId: pollCreation.key.id })
-
-//await client.sendMessage(i.id, { text:" message.from" })
-       // return await sleep(1000)
-           //   }
+	async (message, client, text) => {
+await send_poll(message, client, text);
        }
 );
