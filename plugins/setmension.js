ba9b09a426f505af5bacb80e2fbc3fb10e394c56
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-14 21:29:52 +0530

    Create setmension.js

---
+++ plugins/setmension.js
@@ -0,0 +1,22 @@
+const { inrl } = require('../lib');
+consg img = require('../media/imagee.jpg');
+const fs = require('fs');
+
+inrl(
+	   {
+		pattern: ['jid'],
+		desc: 'To check ping',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+	async (message, client) => {
+		await client.sendMessage(message.from, {image: fs.readFileSync(img), caption: '*Truth*\n\n+ ttrth,' quoted: message,contextInfo: { externalAdReply:{
+        title:"ZIM BOT V4",
+        body:"SUB DRIPS OFC",
+        showAdAttribution: true,
+        mediaType:2,
+        thumbnail: fs.readFileSync(img) ,
+        mediaUrl:`https://youtu.be/KNu-gr2h7bo`, 
+        sourceUrl: `https://youtu.be/KNu-gr2h7bo` }}}, {quoted: message})
+   }
+);
