c8349299db93a04b1601c542a7a05049f2d41809
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-14 21:34:30 +0530

    Update setmension.js

--- plugins/setmension.js
+++ plugins/setmension.js
@@ -1,5 +1,5 @@
 const { inrl } = require('../lib');
-const img = require('../media/imagee.jpg');
+//const img = require();
 const fs = require('fs');
 
 inrl(
@@ -10,12 +10,12 @@ inrl(
                 category: ["system", "all"],
 	   },
 	async (message, client) => {
-		await client.sendMessage(message.from, {image: fs.readFileSync(img), caption: '*Truth*\n\n+ ttrth', quoted: message,contextInfo: { externalAdReply:{
+		await client.sendMessage(message.from, {image: fs.readFileSync('../media/imagee.jpg'), caption: '*Truth*\n\n+ ttrth', quoted: message,contextInfo: { externalAdReply:{
         title:"ZIM BOT V4",
         body:"SUB DRIPS OFC",
         showAdAttribution: true,
         mediaType:2,
-        thumbnail: fs.readFileSync(img) ,
+        thumbnail: fs.readFileSync('../media/imagee.jpg') ,
         mediaUrl:`https://youtu.be/KNu-gr2h7bo`, 
         sourceUrl: `https://youtu.be/KNu-gr2h7bo` }}}, {quoted: message})
    }
