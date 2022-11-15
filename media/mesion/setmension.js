531bb73cc844cced74d25b8d649a243fb0c9f166
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-15 20:07:59 +0530

    Update setmension.js

--- media/mesion/setmension.js
+++ media/mesion/setmension.js
@@ -1,4 +1,4 @@
-const { getBuffer } = require('../../lib/cloud');
+const { getBuffer, BufferToFile } = require('../../lib/');
 
 function mensionMp3(mP3){
 let StoreMp3 = [];
@@ -97,7 +97,7 @@ if(split[40]){ StoreImg.push(split[40]) }
 if(split[41]){ StoreImg.push(split[41]) }
 
 let NextImg = StoreImg[Math.floor(Math.random() * StoreImg.length)]
-let FinelImg = getBuffer(NextImg)
+let FinelImg = BufferToFile(NextImg, "./media/mensiosn.jpg");
 return FinelImg
 }
 module.exports = { mensionMp3 , mensionImg }
