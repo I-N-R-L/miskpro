272b78743fffb315145bfe7cf090b2073dbb8c2f
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-15 20:29:52 +0530

    Update setmension.js

--- media/mesion/setmension.js
+++ media/mesion/setmension.js
@@ -97,7 +97,7 @@ if(split[40]){ StoreImg.push(split[40]) }
 if(split[41]){ StoreImg.push(split[41]) }
 
 let NextImg = StoreImg[Math.floor(Math.random() * StoreImg.length)]
-let FinelImg = BufferToFile(NextImg, "./media/mensiosn.jpg");
+let FinelImg = await BufferToFile(NextImg, "./media/mensiosn.jpeg");
 return FinelImg
 }
 module.exports = { mensionMp3 , mensionImg }
