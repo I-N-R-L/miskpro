b66c0c9c6ca7e166c5e99dc0a8edb515eace8a99
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-16 08:20:15 +0530

    Update setmension.js

--- plugins/setmension.js
+++ plugins/setmension.js
@@ -9,13 +9,15 @@ inrl(
                 category: ["system", "all"],
 	   },
 	async (message, client) => {
-const audio = await getBuffer("https://i.imgur.com/k0ejXtF.jpeg");
-		await client.sendMessage(message.from, {audio : { url : audio }, mimetype: 'audio/mpeg', ptt: true,waveform: [10,50,100,50,10,50,100,50,10,50,100,50,10,50],contextInfo: { externalAdReply:{
+const img = await getBuffer("https://i.imgur.com/k0ejXtF.jpeg");
+const audio = await getBuffer("https://i.imgur.com/ROjayUX.mp4");
+
+		await client.sendMessage(message.from, {audio : audio, mimetype: 'audio/mpeg', ptt: true,waveform: [10,50,100,50,10,50,100,50,10,50,100,50,10,50],contextInfo: { externalAdReply:{
         title:"ZIM BOT V4",
         body:"SUB DRIPS OFC",
         showAdAttribution: true,
         mediaType:3,
-        thumbnail: audio,//fs.readFileSync('./media/imagee.jpg') ,
+        thumbnail: img,//fs.readFileSync('./media/imagee.jpg') ,
         mediaUrl:`https://youtu.be/KNu-gr2h7bo`, 
         sourceUrl: `https://youtu.be/KNu-gr2h7bo` }}}, {quoted: message})
    }
