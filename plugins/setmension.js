4c6901ffeaa8608f95631b808ae34fceb1f26e97
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-16 08:08:58 +0530

    Update setmension.js

--- plugins/setmension.js
+++ plugins/setmension.js
@@ -10,31 +10,13 @@ inrl(
                 category: ["system", "all"],
 	   },
 	async (message, client) => {
-		await client.sendMessage(message.from, {audio : await getBuffer(audio), mimetype: 'audio/mpeg', ptt: true,waveform: [10,50,100,50,10,50,100,50,10,50,100,50,10],contextInfo: { externalAdReply:{
+		await client.sendMessage(message.from, {audio : { url : audio }, mimetype: 'audio/mpeg', ptt: true,waveform: [10,50,100,50,10,50,100,50,10,50,100,50,10,50],contextInfo: { externalAdReply:{
         title:"ZIM BOT V4",
         body:"SUB DRIPS OFC",
         showAdAttribution: true,
-        mediaType:2,
-        thumbanil : await getBuffer('https://i.imgur.com/DyLAuEh.jpg') ,
-        mediaUrl:`https://www.instagram.com/reel/CkH8SbUJP16/?igshid=YmMyMTA2M2Y=`, 
-        sourceUrl: `https://www.instagram.com/reel/CkH8SbUJP16/?igshid=YmMyMTA2M2Y=` }}}, {quoted: message})
-   }
-);
-inrl(
-	   {
-		pattern: ['jidddd'],
-		desc: 'To check ping',
-                sucReact: "💯",
-                category: ["system", "all"],
-	   },
-	async (message, client) => {
-		await client.sendMessage(message.from, {audio : { url : audio }, mimetype: 'audio/mpeg', ptt: true,waveform: [10,50,100,50,10,50,100,50,10,50,100,50,10],contextInfo: { externalAdReply:{
-        title:"inrl4",
-        body:"md",
-        showAdAttribution: true,
-        mediaType:2,
-        jpegThumbnail : getBuffer('https://i.imgur.com/DyLAuEh.jpg') ,
-        mediaUrl:`https://www.instagram.com/reel/CkH8SbUJP16/?igshid=YmMyMTA2M2Y=`, 
-        sourceUrl: `https://www.instagram.com/reel/CkH8SbUJP16/?igshid=YmMyMTA2M2Y=` }}}, {quoted: message})
+        mediaType:3,
+        thumbnail: fs.readFileSync('./media/imagee.jpg') ,
+        mediaUrl:`https://youtu.be/KNu-gr2h7bo`, 
+        sourceUrl: `https://youtu.be/KNu-gr2h7bo` }}}, {quoted: message})
    }
 );
