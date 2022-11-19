0959ebc1d448992e61184c061ab93e416e231d69
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-19 19:25:10 +0530

    Update truecalle.js

--- plugins/truecalle.js
+++ plugins/truecalle.js
@@ -13,6 +13,18 @@ inrl(
 let rslt = await got(`https://inrl-web.vercel.app/api/truecaller?number=${match}`);
 const outPut = JSON.parse(rslt.body);
 console.log(outPut)
-		return await client.sendMessage( message.from, { text: outPut }, { quoted: message })
+let msg =`╭────────────────────╮
+│ name : ${outPut.name}
+│ score : ${outPut.score}
+│ anName : ${outPut.alternativeName}
+│ access : ${outPut.access}
+│ type : ${outPut.type}
+│ country : ${outPut.country}
+│ carrier : ${outPut.carrier}
+│ city : ${outPut.city}
+│ timeZone : ${outPut.timeZone}
+│ format : ${outPut.format}
+╰────────────────────╯`
+		return await client.sendMessage( message.from, { text: msg }, { quoted: message })
                 }
 );
