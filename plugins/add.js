be7f6dafd846c47a6d90549cea9c828528a08f09
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-20 22:42:50 +0530

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -257,6 +257,6 @@ inrl({ pattern: ["pp"],desc: 'set  profile picture of bot', sucReact: "😁",  c
 	async (message, client) => {
 	//let _message = message.quoted.imageMessage || message.client.text;
 		let download = await message.quoted.download();
-		await client.updateProfilePicture({ renderLargerThumbnail : true },message.client.botNumber,download ).catch((err) => fs.unlinkSync(download))
+		await client.updateProfilePicture(message.client.botNumber,download,{ renderLargerThumbnail : true } ).catch((err) => fs.unlinkSync(download))
       }
 );
