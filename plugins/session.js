3e1640a308faf95e973328870b5d88af9303b517
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-21 10:15:24 +0530

    Update session.js

--- plugins/session.js
+++ plugins/session.js
@@ -5,6 +5,11 @@ const fs = require('fs');
 const { exec, spawn, execSync } = require('child_process')
 async function photoGen(message,client){
 return new Promise(async(resolve,reject) => {
+if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
+   if (!/webp/.test(message.client.mime)) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
+let _message = message.quoted.stickerMessage ;
+   let media = await client.downloadAndSaveMediaMessage(_message);
+   let ran = await getRandom('.png')
   let photo = exec(`ffmpeg -i ${media} ${ran}`, (err) => {
   //fs.unlinkSync(media)
   if (err) client.sendMessage(message.from, { text: "err"+err }, { quoted: message });
@@ -17,11 +22,6 @@ return new Promise(async(resolve,reject) => {
 }
 
 inrl({ pattern: ['photo','toimg'], desc: "to convert webp to img",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
-   if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
-   if (!/webp/.test(message.client.mime)) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
-let _message = message.quoted.stickerMessage ;
-   let media = await client.downloadAndSaveMediaMessage(_message);
-   let ran = await getRandom('.png')
 let img = await photoGen(message,client);
   client.sendMessage(message.from, { image:  img , caption: "config.exif.cap" }, { quoted: message });
   //unlink(ran)
