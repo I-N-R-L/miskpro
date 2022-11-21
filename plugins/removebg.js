44c99d7cf459b7c4bebc814e0ecd32691a7e57ca
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-21 21:15:30 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -14,7 +14,7 @@ if(message.quoted.imageMessage){
 let img = await client.downloadAndSaveMediaMessage(message.quoted.imageMessage)
 let rmbgimg = await remove(fs.readFileSync(img))
 let rmbg = await fs.writeFileSync('./media/rmbg/isexit.jpg', rmbgimg)
-await client.sendMessage( message.from, { image : rmbg }, caption : Config.CAPTION, { quoted: message })
+await client.sendMessage( message.from, { image : rmbg, caption : Config.CAPTION }, { quoted: message })
 await fs.unlinkSync(img);return await fs.unlinkSync(rmbg);
                 }
         }
