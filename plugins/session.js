b60d74321e076dd03d918bf221e72789fed8052a
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-21 09:42:36 +0530

    Tesyjsjs

---
+++ plugins/session.js
@@ -0,0 +1,18 @@
+const { inrl } = require('../lib');
+const ffmpeg = require('fluent-ffmpeg')
+const {unlinkSync,readFileSync} = require('fs').promises;
+
+inrl({ pattern: ['photo','toimg'], desc: "to convert webp to img",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
+   if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
+   if (!/webp/.test(message.client.mime)) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
+let _message = message.quoted.stickerMessage ;
+   let media = await client.downloadAndSaveMediaMessage(_message);
+   let ran = await getRandom('.png')
+   exec(`ffmpeg -i ${media} ${ran}`, (err) => {
+  unlinkSync(media)
+  if (err) client.sendMessage(message.from, { text: err }, { quoted: message });
+  let buffer = readFileSync(ran)
+  client.sendMessage(message.from, { image:  buffer , caption: bots.config.exif.cap }, { quoted: message });
+  unlinkSync(ran)
+   })
+ });
