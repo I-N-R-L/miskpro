3048f5c79ffe258146d9e34ef0f6b63f93d9f603
multidivicebotforinrl-qrcodeforinrlalso <109638623+multidivicebotforinrl-qrcodeforinrlalso@users.noreply.github.com>
2022-11-21 16:57:16 +0530

    Update Ffmpg.js

--- plugins/Ffmpg.js
+++ plugins/Ffmpg.js
@@ -10,7 +10,7 @@ inrl(
     category: ["search"] 
 },
   async (message, client) => {
-let _message = message.quoted.imageMessage;
+let _message = message.quoted.audioMessage;
   var media = await client.downloadMediaMessage(_message);
         ffmpeg(media).outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"]).save('output.mp4').on('end', async () => {
         var res = ('output.mp4');
