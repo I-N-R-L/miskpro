6995f1cf08b585509128aa6bb1042c8ceb047e5a
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-21 22:19:19 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -19,3 +19,35 @@ await fs.unlinkSync(img);//return await fs.unlinkSync(rmbg);
                 }
         }
 );
+inrl(
+  {
+    pattern: ["img"],
+    usage: '<text>',
+    sucReact: "🖼",
+    category: ["search", "all"],
+  },
+  async (message, client) => {
+    const text = message.client.text;
+    if (!text) {
+      global.catchError = true;
+      return await client.sendMessage( message.from, { text: bots.errorMessage('Enter Text') }, { quoted: message } );
+    }
+
+    gis(text, async (error, results) => {
+        if (error) {
+          global.catchError = true;
+          return await client.sendErrorMessage( message.from, error, message.key, message );
+        } else {
+          let data = results.length
+          let img = results[Math.floor(data * Math.random())]
+console.log(img.url);
+            await client.sendMessage( message.from, { image: { url: results[i].url }, caption: bots.config.exif.cap,}, { quoted: message, });
+            global.catchError = false;
+        }
+    
+      }).catch(async (err) => {
+        (global.catchError = true),
+          await client.sendErrorMessage( message.from, err, message.key, message );
+      });
+  }
+);
