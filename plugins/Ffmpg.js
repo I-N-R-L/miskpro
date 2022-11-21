3c9db4e0f93c003a133008407e813e7f24717215
multidivicebotforinrl-qrcodeforinrlalso <109638623+multidivicebotforinrl-qrcodeforinrlalso@users.noreply.github.com>
2022-11-21 16:49:48 +0530

    Update and rename ytdownlode.js to Ffmpg.js

--- plugins/Ffmpg.js
+++ plugins/Ffmpg.js
@@ -12,5 +12,5 @@ inrl(
         ffmpeg(media).outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"]).save('output.mp4').on('end', async () => {
         var res = ('output.mp4');
         await client.reply("res.link");
-   }
-);
+   });
+});
