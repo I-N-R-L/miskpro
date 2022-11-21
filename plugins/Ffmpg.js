9c4f0ea74f1c993d021ca3b9c0d41a05664325eb
multidivicebotforinrl-qrcodeforinrlalso <109638623+multidivicebotforinrl-qrcodeforinrlalso@users.noreply.github.com>
2022-11-21 17:14:35 +0530

    Update Ffmpg.js

--- plugins/Ffmpg.js
+++ plugins/Ffmpg.js
@@ -11,8 +11,8 @@ inrl(
 },
   async (message, client) => {
 let _message = message.quoted.audioMessage;
-  var media = await client.downloadAndSaveMediaMessage(_message);
-        ffmpeg(media).outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"]).save('output.mp4').on('end', async () => {
+        let media = await client.downloadAndSaveMediaMessage(_message);
+        await ffmpeg(media).outputOptions(["-y", "-filter_complex", "[0:a]showvolume=f=1:b=4:w=720:h=68,format=yuv420p[vid]", "-map", "[vid]", "-map 0:a"]).save('output.mp4').on('end', async () => {
         var res = ('output.mp4');
         await client.reply("res.link");
    });
