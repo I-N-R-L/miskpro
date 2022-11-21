f2e1c8ee149201890be004f89646c540bb6c256b
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-21 09:44:02 +0530

    Update session.js

--- plugins/session.js
+++ plugins/session.js
@@ -1,6 +1,7 @@
-const { inrl } = require('../lib');
+const { inrl, getRandom } = require('../lib');
 const ffmpeg = require('fluent-ffmpeg')
 const {unlinkSync,readFileSync} = require('fs').promises;
+const { exec, spawn, execSync } = require('child_process')
 
 inrl({ pattern: ['photo','toimg'], desc: "to convert webp to img",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
    if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
