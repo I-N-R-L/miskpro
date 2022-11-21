002c85aa9affeeb01b4978e7febc052142d5280f
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-21 09:46:38 +0530

    Update session.js

--- plugins/session.js
+++ plugins/session.js
@@ -1,6 +1,7 @@
 const { inrl, getRandom } = require('../lib');
 const ffmpeg = require('fluent-ffmpeg')
-const {unlinkSync,readFileSync} = require('fs').promises;
+const {readFileSync} = require('fs').promises;
+const fs = require('fs');
 const { exec, spawn, execSync } = require('child_process')
 
 inrl({ pattern: ['photo','toimg'], desc: "to convert webp to img",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
@@ -10,10 +11,10 @@ let _message = message.quoted.stickerMessage ;
    let media = await client.downloadAndSaveMediaMessage(_message);
    let ran = await getRandom('.png')
    exec(`ffmpeg -i ${media} ${ran}`, (err) => {
-  unlinkSync(media)
+  fs.unlinkSync(media)
   if (err) client.sendMessage(message.from, { text: err }, { quoted: message });
   let buffer = readFileSync(ran)
   client.sendMessage(message.from, { image:  buffer , caption: bots.config.exif.cap }, { quoted: message });
-  unlinkSync(ran)
+  fs.unlinkSync(ran)
    })
  });
