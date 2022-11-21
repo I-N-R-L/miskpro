9c20e1979183c885f97fd7096d8493ccb3d922c9
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-21 10:05:54 +0530

    Update session.js

--- plugins/session.js
+++ plugins/session.js
@@ -3,6 +3,15 @@ const ffmpeg = require('fluent-ffmpeg')
 const {readFile,unlink} = require('fs').promises;
 const fs = require('fs');
 const { exec, spawn, execSync } = require('child_process')
+async function photoGen(message,client){
+return new Promise(async(resolve,reject) => {
+  let photo = exec(`ffmpeg -i ${media} ${ran}`, (err) => {
+  //fs.unlinkSync(media)
+  if (err) client.sendMessage(message.from, { text: "err"+err }, { quoted: message });
+  buffer = fs.readFileSync(ran)
+  await resolve buffer;
+  })
+}
 
 inrl({ pattern: ['photo','toimg'], desc: "to convert webp to img",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
    if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a sticker"},{ quoted: message })
@@ -10,11 +19,7 @@ inrl({ pattern: ['photo','toimg'], desc: "to convert webp to img",sucReact: "⚒
 let _message = message.quoted.stickerMessage ;
    let media = await client.downloadAndSaveMediaMessage(_message);
    let ran = await getRandom('.png')
-   exec(`ffmpeg -i ${media} ${ran}`, (err) => {
-  //fs.unlinkSync(media)
-  if (err) client.sendMessage(message.from, { text: "err"+err }, { quoted: message });
-  let buffer = readFile(ran)
-  client.sendMessage(message.from, { image:  buffer , caption: "config.exif.cap" }, { quoted: message });
+let img = await photoGen(message,client);
+  client.sendMessage(message.from, { image:  img , caption: "config.exif.cap" }, { quoted: message });
   //unlink(ran)
    })
- });
