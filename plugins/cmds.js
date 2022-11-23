728c7c8ce97854158df437ddbd55d52d6534aa3e
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-23 10:18:29 +0530

    Update cmds.js

--- plugins/cmds.js
+++ plugins/cmds.js
@@ -1,9 +1,6 @@
 const { inrl } = require('../lib');
 inrl({ pattern: ["cmds"], usage: '<mentions|reply>', sucReact: "🤐", category: ["group", "all"], },
   async (message, client) => {
-await client.sendMessage(message.from, { text : `args = ${message.client.args}
-withcmd = ${message.client.withcmd}
-withoutcmd = ${message.clinet.withoutcmd}
-text = ${message.client.text}`
+await client.sendMessage(message.from, { text : `args = ${message.client.args}`
     })
 })
