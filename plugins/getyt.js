ff3c80a64ad5438b2a6da35fed276d84cb951963
multidivicebotforinrl-qrcodeforinrlalso <109638623+multidivicebotforinrl-qrcodeforinrlalso@users.noreply.github.com>
2022-11-23 16:02:36 +0530

    Update getyt.js

--- plugins/getyt.js
+++ plugins/getyt.js
@@ -3,12 +3,27 @@ const { getYtV } = require('../lib/youtube');
 //const { getLastMessageInChat } = require('@adiwajshing/baileys');
 inrl(
 	   {
-		pattern: ['ytv'],
+		pattern: ['ytmp4'],
 		desc: 'To check ping',
                 sucReact: "💯",
                 category: ["system", "all"],
 	   },
-	async (message, client) => {
+	async (message, client,match) => {
+if(!match.includes('http')){
 await getYtV(message, client)
+}
+         }
+);
+inrl(
+	   {
+		pattern: ['ytmp3'],
+		desc: 'To check ping',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+	async (message, client,match) => {
+if(!match.includes('http')){
+await getYtVA(message, client)
+}
          }
 );
