428fb59f89b7ce77279721c5ab835975cfc4c729
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-27 10:38:05 +0530

    Update sedrpt.js

--- plugins/sedrpt.js
+++ plugins/sedrpt.js
@@ -10,5 +10,5 @@ inrl(
 if(!match) return;
 let number = match.split(',')[0] || match;
 let type = match.split(',')[1] || "text";
- await sendRepeat(number, m, c, type)
+ await sendRepeat(number, message, client, type)
 });
