618d1cab55628e069efe853f21433338df9a0a76
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-29 11:26:33 +0530

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -7,8 +7,10 @@ inrl(
                 category: ["system", "all"],
 	   },
 	async (message, client) => {
-const groupAdmins = message.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
+        const groupMetadata = message.isGroup ? await client.groupMetadata(message.from).catch(e => {}) : ''
+	const participants = message.isGroup ? await groupMetadata.participants : ''
+        const groupAdmins = message.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
 	return message.isGroup ? groupAdmins.includes(message.user_id) : false
-console.log(groupAdmins);
+console.log(groupAdmins,participants);
 console.log(message.groupAdmins)
 });
