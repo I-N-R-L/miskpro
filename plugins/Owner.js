159997e2a7902a215ea46cc48e78f7319f9a1355
intkfthhffffghhbvfdsscghhnkkjjjjhhgg <107807702+intkfthhffffghhbvfdsscghhnkkjjjjhhgg@users.noreply.github.com>
2022-11-29 16:18:07 +0530

    Update Owner.js

--- plugins/Owner.js
+++ plugins/Owner.js
@@ -9,29 +9,3 @@ const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
  inrl({pattern: ['owner'], desc: "to check whether", sucReact: "🥺", category: ['all']},   async (message, client) => {
  await client.sendMessage( message.from, { contacts:{ displayName:`${Config.BOT_INFO.split(",")[0]}`, contacts: [{ vcard }],}})
 });
-inrl(
-  {
-    pattern: ["script"],
-    desc: "to check the bot status",
-    sucReact: "🥵",
-    category: ["system", "all"],
-  },
-  async (message, client) => {
-      const Message = {
-      image: { url: config.image.url.D_E_TMB },
-      caption: `╭═══〘${Config.BOT_INFO.split(",")[0]}〙═══⊷❍
-┃☯︎╭──────────────
-┃☯︎│
-┃☯︎│ ᴏᴡɴᴇʀ :${Config.BOT_INFO.split(",")[1]}
-┃☯︎│ ᴜꜱᴇʀ : ${message.client.pushName}
-┃☯︎│ ᴠᴇʀꜱɪᴏɴ : ${Config.VERSION}
-┃☯︎│ ɢɪᴛʜᴜʙ :`+Config.GIT+`
-┃☯︎│ ᴡᴇʙꜱɪᴛᴇ :`+Config.WEB+`
-┃☯︎│ ᴛᴜʀᴛᴏʀɪᴀʟ :`+Config.VIDEO+`
-┃☯︎│ yᴏᴜᴛᴜʙᴇ :`+Config.YT+`
-┃☯︎│
-┃☯︎╰───────────────
-╰═════════════════⊷`
-    };
-    await client.sendMessage(message.from, Message, { quoted: message });
-});
