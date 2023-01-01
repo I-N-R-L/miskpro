362c09204ef61bf8d3e6fe7df8227dc6e25bd48f
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-01-01 16:56:46 +0530

    Delete global.js

--- global.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs")
-global.owner = ["917593919575"];
-global.mess = (type, m) => {
-    let msg = { wait: 'Wait, in progress', owner: 'Perintah ini hanya dapat digunakan oleh Owner!', premium: 'Perintah ini hanya dapat digunakan oleh Premium!', group: 'Perintah ini hanya dapat digunakan di group!', private: 'Perintah ini hanya dapat digunakan di private chat!', admin: 'Perintah ini hanya dapat digunakan oleh admin group!', botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut', bot: 'Fitur ini hanya dapat diakses oleh Bot', dead: 'Fitur ini sedang dimatikan!', media: 'Reply media', error: "No Results Found" }[type]
-    if (msg) return m.reply(msg, m.from, { quoted: m })
-}
